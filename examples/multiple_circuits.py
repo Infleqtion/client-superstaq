277ebae215073a4225fc9c24ff9fe3859684089e
Rohan Kumar <rohansk@uchicago.edu>
2021-08-11 23:58:09 +0800

    --

--- examples/multiple_circuits.py
+++ examples/multiple_circuits.py
@@ -1,11 +1,17 @@
+"""
+Creates and simulates multiple circuits with batch submission.
+
+
+"""
+
 import qiskit
 import qiskit_superstaq as qss
 
 # SuperstaQ token retrieved through API
-token = "insert API token"
+token = "ya29.a0ARrdaM9KXhhrf1dejww05FO4L7ZNc7HDiw8Un15xVfjUmMxDBdZZd1xuKHHzrxG4LC6-RRH6MdT4cg-bA94uG1TejdV0hmI7AP-RO7pEQdA3AG-uGNw9CR8ArBCzZNT3B0QJVjw1HjH3BTzS8Khiick9q9_V"
 
 # Create provider using authorization token
-superstaq = qss.superstaq_provider.SuperstaQProvider(token)
+superstaq = qss.superstaq_provider.SuperstaQProvider(token, url = "https://127.0.0.1:5000")
 
 # Retrieve backend from superstaq provider
 backend = superstaq.get_backend("ibmq_qasm_simulator")
@@ -24,10 +30,10 @@ qc2.cx(0, 2)
 qc2.measure([0, 1, 2], [0, 1, 2])
 
 
-"""
-Submit list of jobs to backend. Circuits submitted simultaneously
-will run with the same backend and the same number of shots.
-"""
+
+# Submit list of jobs to backend. Circuits submitted simultaneously
+# will run with the same backend and the same number of shots.
+
 job = backend.run([qc1, qc2], shots=100)
 
 # List of result counts
