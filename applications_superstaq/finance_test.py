5581678080bd042441a927b14c504a06b1d69067
Teague Tomesh <ttomesh@princeton.edu>
2021-08-23 13:18:47 -0500

    Update tests

--- applications_superstaq/finance_test.py
+++ applications_superstaq/finance_test.py
@@ -1,17 +1,21 @@
 import finance
+import qubo
+import qubovert as qv
 
 
 def test_read_json_minvol() -> None:
     best_portfolio = ["AAPL", "GOOG"]
     best_ret = 8.1
     best_std_dev = 10.5
+    qubo_obj = qv.QUBO({("0", "1"): -1.0})
     json_dict = {
         "best_portfolio": best_portfolio,
         "best_ret": best_ret,
         "best_std_dev": best_std_dev,
+        "qubo": qubo.convert_qubo_to_model(qubo_obj),
     }
     assert finance.read_json_minvol(json_dict) == finance.MinVolOutput(
-        best_portfolio, best_ret, best_std_dev
+        best_portfolio, best_ret, best_std_dev, qubo_obj
     )
 
 
@@ -20,12 +24,14 @@ def test_read_json_maxsharpe() -> None:
     best_ret = 8.1
     best_std_dev = 10.5
     best_sharpe_ratio = 0.771
+    qubo_obj = qv.QUBO({("0", "1"): -1.0})
     json_dict = {
         "best_portfolio": best_portfolio,
         "best_ret": best_ret,
         "best_std_dev": best_std_dev,
         "best_sharpe_ratio": best_sharpe_ratio,
+        "qubo": qubo.convert_qubo_to_model(qubo_obj),
     }
     assert finance.read_json_maxsharpe(json_dict) == finance.MaxSharpeOutput(
-        best_portfolio, best_ret, best_std_dev, best_sharpe_ratio
+        best_portfolio, best_ret, best_std_dev, best_sharpe_ratio, qubo_obj
     )
--- applications_superstaq/logistics_test.py
+++ applications_superstaq/logistics_test.py
@@ -1,4 +1,6 @@
 import logistics
+import qubo
+import qubovert as qv
 
 
 def test_read_json_tsp() -> None:
@@ -6,14 +8,16 @@ def test_read_json_tsp() -> None:
     route_list_numbers = [0, 1, 2, 0]
     total_distance = 100.0
     map_link = ["maps.google.com"]
+    qubo_obj = qv.QUBO({("0", "1"): -1.0})
     json_dict = {
         "route": route,
         "route_list_numbers": route_list_numbers,
         "total_distance": total_distance,
         "map_link": map_link,
+        "qubo": qubo.convert_qubo_to_model(qubo_obj),
     }
     assert logistics.read_json_tsp(json_dict) == logistics.TSPOutput(
-        route, route_list_numbers, total_distance, map_link
+        route, route_list_numbers, total_distance, map_link, qubo_obj
     )
 
 
@@ -22,12 +26,14 @@ def test_read_json_warehouse() -> None:
     total_distance = 100.0
     map_link = "map.html"
     open_warehouses = ["Chicago"]
+    qubo_obj = qv.QUBO({("0", "1"): -1.0})
     json_dict = {
         "warehouse_to_destination": warehouse_to_destination,
         "total_distance": total_distance,
         "map_link": map_link,
         "open_warehouses": open_warehouses,
+        "qubo": qubo.convert_qubo_to_model(qubo_obj),
     }
     assert logistics.read_json_warehouse(json_dict) == logistics.WarehouseOutput(
-        warehouse_to_destination, total_distance, map_link, open_warehouses
+        warehouse_to_destination, total_distance, map_link, open_warehouses, qubo_obj
     )
