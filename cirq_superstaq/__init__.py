89b645e3e033138f41ef21ad08c7142741e98c4d
rich <rich@super.tech>
2021-08-26 18:11:25 -0500

    fix import order

--- cirq_superstaq/__init__.py
+++ cirq_superstaq/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cirq_superstaq._init_vars import API_URL, API_VERSION
 from cirq_superstaq import custom_gates
+from cirq_superstaq._init_vars import API_URL, API_VERSION
 from cirq_superstaq.job import Job
 from cirq_superstaq.service import Service
 from cirq_superstaq.superstaq_exceptions import (
