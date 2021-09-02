564cc46b778aa55bb502596a6c408f94ddc234e5
rich <rich@super.tech>
2021-09-02 15:27:02 -0500

    add (de)serialize() functions

--- applications_superstaq/converters.py
+++ applications_superstaq/converters.py
@@ -1,9 +1,38 @@
 import codecs
+import pickle
 
 
-def bytes_to_str(bytes_data: bytes) -> str:
+def _bytes_to_str(bytes_data: bytes) -> str:
     return codecs.encode(bytes_data, "base64").decode()
 
 
-def str_to_bytes(str_data: str) -> bytes:
+def _str_to_bytes(str_data: str) -> bytes:
     return codecs.decode(str_data.encode(), "base64")
+
+
+def serialize(obj: Any) -> str:
+    """Serialize picklable object into a string
+
+    Args:
+        obj: a picklable object to be serialized
+
+    Returns:
+        str representing the serialized object
+    """
+
+    return _bytes_to_str(pickle.dumps(obj))
+
+
+def deserialize(serialized_obj: str) -> Any:
+    """Deserialize serialized objects
+
+    Args:
+        serialized_obj: a str generated via qiskit_superstaq.converters.serialize()
+
+    Returns:
+        the serialized object
+    """
+
+    return pickle.loads(_str_to_bytes(serialized_obj))
+
+
--- applications_superstaq/converters_test.py
+++ applications_superstaq/converters_test.py
@@ -1,10 +1,8 @@
-import pickle
-
 import applications_superstaq
 
 
-def test_bytes_str_conversions() -> None:
-    str_data = applications_superstaq.converters.bytes_to_str(pickle.dumps("test_data"))
-    assert isinstance(str_data, str)
-    bytes_data = applications_superstaq.converters.str_to_bytes(str_data)
-    assert pickle.loads(bytes_data) == "test_data"
+def test_serialization() -> None:
+    obj = {"object": ["to", "serialize"]}
+    serialized_obj = qiskit_superstaq.converters.serialize(obj)
+    assert isinstance(serialized_obj, str)
+    assert qiskit_superstaq.converters.deserialize(serialized_obj) == obj
