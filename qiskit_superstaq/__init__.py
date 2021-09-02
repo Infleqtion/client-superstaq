cdae9b1e13d6c475a5fde47763c2639317505efa
rich <rich@super.tech>
2021-09-02 18:09:21 -0500

    typo

--- qiskit_superstaq/__init__.py
+++ qiskit_superstaq/__init__.py
@@ -1,5 +1,5 @@
 from ._init_vars import API_URL, API_VERSION
-from . import serializaton  # noqa: I100; b/c ._init_vars need to be init first
+from . import serialization  # noqa: I100; b/c ._init_vars need to be init first
 from . import superstaq_backend
 from . import superstaq_job
 from . import superstaq_provider
@@ -7,7 +7,7 @@ from . import superstaq_provider
 __all__ = [
     "API_URL",
     "API_VERSION",
-    "serializaton",
+    "serialization",
     "superstaq_backend",
     "superstaq_job",
     "superstaq_provider",
