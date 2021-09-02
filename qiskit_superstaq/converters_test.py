d08cf62ff3fb980716be6787df9b30c24ddec9e3
rich <rich@super.tech>
2021-09-02 14:03:30 -0500

    add test

--- qiskit_superstaq/converters_test.py
+++ qiskit_superstaq/converters_test.py
@@ -4,6 +4,13 @@ import qiskit_superstaq
 
 
 def test_serialization() -> None:
+    obj = {"object": ["to", "serialize"]}
+    serialized_obj = qiskit_superstaq.converters.serialize(obj)
+    assert isinstance(serialized_obj, str)
+    assert qiskit_superstaq.converters.deserialize(serialized_obj) == obj
+
+
+def test_circuit_serialization() -> None:
     circuits = [qiskit.QuantumCircuit(3), qiskit.QuantumCircuit(2)]
     circuits[0].cx(2, 1)
     circuits[0].cz(0, 1)
