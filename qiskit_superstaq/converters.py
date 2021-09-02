90d3f1ea8766614a8e9090bfcb7dcb908794d3bc
rich <rich@super.tech>
2021-09-02 13:01:14 -0500

    docstrings

--- qiskit_superstaq/converters.py
+++ qiskit_superstaq/converters.py
@@ -15,11 +15,27 @@ def str_to_bytes(str_data: str) -> bytes:
 
 
 def serialize_circuits(circuits: Union[qiskit.QuantumCircuit, List[qiskit.QuantumCircuit]]) -> str:
+    """Serialize QuantumCircuits into a single string
+
+    Args:
+        circuits: a QuantumCircuit or list of QuantumCircuits to be serialized
+
+    Returns:
+        str representing the serialized circuits
+    """
     buf = io.BytesIO()
     qiskit.circuit.qpy_serialization.dump(circuits, buf)
     return bytes_to_str(buf.getvalue())
 
 
 def deserialize_circuits(serialized_circuits: str) -> List[qiskit.QuantumCircuit]:
+    """Deserialize serialized QuantumCircuit(s)
+
+    Args:
+        serialized_circuits: str generated via qiskit_superstaq.converters.serialize_circuit()
+
+    Returns:
+        a list of QuantumCircuits
+    """
     buf = io.BytesIO(str_to_bytes(serialized_circuits))
     return qiskit.circuit.qpy_serialization.load(buf)
