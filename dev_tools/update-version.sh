2fd7d942e49b4c358d83e42b8adf4f1b8c87a6e8
dowusu-antwi <david.owusuantwi@gmail.com>
2021-10-05 19:20:52 -0500

    Publish to pypi (#74)
    * editing main
    
    * adds quotes to env variable
    
    Co-authored-by: David Owusu-Antwi <david@super.tech>

--- dev_tools/update-version.sh
+++ dev_tools/update-version.sh
@@ -4,4 +4,4 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$(git rev-parse --show-toplevel)"
 
 python setup.py bdist_wheel
-twine upload dist/* -u __token__ -p $PYPI_API_KEY
+twine upload dist/* -u __token__ -p "$PYPI_API_KEY"
