8b13d271df08b5efed35f699b94b9b746688106c
Pranav Gokhale <pranav@super.tech>
2021-10-15 13:03:33 -0500

    Add HTTP headers to make client known to server

--- cirq_superstaq/superstaq_client.py
+++ cirq_superstaq/superstaq_client.py
@@ -89,7 +89,12 @@ class _SuperstaQClient:
         self.verify_https: bool = (
             cirq_superstaq.API_URL + "/" + cirq_superstaq.API_VERSION == self.url
         )
-        self.headers = {"Authorization": api_key, "Content-Type": "application/json"}
+        self.headers = {
+            "Authorization": api_key,
+            "Content-Type": "application/json",
+            "X-Client-Version": cirq_superstaq.API_VERSION,
+            "X-Client-Name": "cirq-superstaq",
+        }
         self.default_target = default_target
         self.max_retry_seconds = max_retry_seconds
         self.verbose = verbose
