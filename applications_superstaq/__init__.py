73ee922065e2d2f14c66987f9be91527e49e3b6d
vtomole <vtomole2@gmail.com>
2021-11-10 15:20:01 -0600

    Add git configs

--- applications_superstaq/__init__.py
+++ applications_superstaq/__init__.py
@@ -2,6 +2,8 @@ from applications_superstaq._init_vars import API_URL, API_VERSION
 from . import converters
 from . import finance
 from . import logistics
+from . import superstaq_client
+from . import superstaq_exceptions
 from . import qubo
 
 __all__ = [
@@ -10,5 +12,7 @@ __all__ = [
     "converters",
     "finance",
     "logistics",
+    "superstaq_client",
+    "superstaq_exceptions",
     "qubo",
 ]
