fc14d4a38859ca8bd6b80b7d169dd07748cc0d1c
rich <rich@super.tech>
2021-12-03 21:51:42 -0600

    use custom_resolver to deserialize custom gates

--- qiskit_superstaq/serialization.py
+++ qiskit_superstaq/serialization.py
@@ -5,6 +5,8 @@ import applications_superstaq
 import qiskit
 import qiskit.circuit.qpy_serialization
 
+import qiskit_superstaq
+
 
 def serialize_circuits(circuits: Union[qiskit.QuantumCircuit, List[qiskit.QuantumCircuit]]) -> str:
     """Serialize QuantumCircuit(s) into a single string
@@ -30,4 +32,12 @@ def deserialize_circuits(serialized_circuits: str) -> List[qiskit.QuantumCircuit
         a list of QuantumCircuits
     """
     buf = io.BytesIO(applications_superstaq.converters._str_to_bytes(serialized_circuits))
-    return qiskit.circuit.qpy_serialization.load(buf)
+    circuits = qiskit.circuit.qpy_serialization.load(buf)
+
+    for circuit in circuits:
+        for pc, (inst, qargs, cargs) in enumerate(circuit._data):
+            new_inst = qiskit_superstaq.custom_gates.custom_resolver(inst)
+            if new_inst is not None:
+                circuit._data[pc] = (new_inst, qargs, cargs)
+
+    return circuits
--- qiskit_superstaq/serialization_test.py
+++ qiskit_superstaq/serialization_test.py
@@ -4,10 +4,13 @@ import qiskit_superstaq
 
 
 def test_circuit_serialization() -> None:
-    circuits = [qiskit.QuantumCircuit(3), qiskit.QuantumCircuit(2)]
+    circuits = [qiskit.QuantumCircuit(3), qiskit.QuantumCircuit(3)]
     circuits[0].cx(2, 1)
-    circuits[0].cz(0, 1)
+    circuits[0].rz(1.23, 0)
     circuits[1].swap(0, 1)
+    circuits[1].append(qiskit_superstaq.AceCR("-+"), [2, 1])
+    circuits[1].append(qiskit_superstaq.FermionicSWAPGate(4.56), [0, 2])
+
     serialized_circuits = qiskit_superstaq.serialization.serialize_circuits(circuits)
     assert isinstance(serialized_circuits, str)
     assert qiskit_superstaq.serialization.deserialize_circuits(serialized_circuits) == circuits
