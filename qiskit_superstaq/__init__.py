f0f6a797dda195a6fc322ccd2e07aa4869519baf
Victory Omole <vtomole2@gmail.com>
2022-01-26 11:09:02 -0600

    Add version to init file (#93)

--- qiskit_superstaq/__init__.py
+++ qiskit_superstaq/__init__.py
@@ -3,6 +3,7 @@ from . import serialization  # noqa: I100; b/c ._init_vars need to be init first
 from . import superstaq_backend
 from . import superstaq_job
 from . import superstaq_provider
+from ._version import __version__
 from .custom_gates import AceCR, FermionicSWAPGate, ParallelGates
 
 __all__ = [
@@ -15,4 +16,5 @@ __all__ = [
     "superstaq_backend",
     "superstaq_job",
     "superstaq_provider",
+    "__version__",
 ]
