c56fe2216427567879308ff78cebb7a60444c13c
vtomole <vtomole2@gmail.com>
2022-06-18 15:07:12 -0400

    isolate qiskit imports to converters.py

--- supermarq/features_test.py
+++ supermarq/features_test.py
@@ -1,15 +1,8 @@
 import cirq
-import qiskit
 
 import supermarq as sm
 
-
-qiskit_circuit = qiskit.QuantumCircuit(2, 1)
-qiskit_circuit.swap(0, 1)
-qiskit_circuit.measure(0, 0)
-qiskit_circuit.reset(0)
-qiskit_circuit.measure_all()
-cirq_circuit = cirq.Circuit(
+CIRCUIT = cirq.Circuit(
     cirq.SWAP(*cirq.LineQubit.range(2)),
     cirq.measure(cirq.LineQubit(0)),
     cirq.reset(cirq.LineQubit(0)),
@@ -18,46 +11,31 @@ cirq_circuit = cirq.Circuit(
 
 
 def test_compute_communication() -> None:
-    qiskit_feature = sm.features.compute_communication(qiskit_circuit)
-    cirq_feature = sm.features.compute_communication(cirq_circuit)
-    assert qiskit_feature >= 0 and qiskit_feature <= 1
-    assert cirq_feature >= 0 and cirq_feature <= 1
+    feature = sm.features.compute_communication(CIRCUIT)
+    assert feature >= 0 and feature <= 1
 
 
 def test_compute_liveness() -> None:
-    qiskit_feature = sm.features.compute_liveness(qiskit_circuit)
-    cirq_feature = sm.features.compute_liveness(cirq_circuit)
-    assert qiskit_feature >= 0 and qiskit_feature <= 1
-    assert cirq_feature >= 0 and cirq_feature <= 1
+    feature = sm.features.compute_liveness(CIRCUIT)
+    assert feature >= 0 and feature <= 1
 
 
 def test_compute_parallelism() -> None:
-    qiskit_feature = sm.features.compute_parallelism(qiskit_circuit)
-    cirq_feature = sm.features.compute_parallelism(cirq_circuit)
-    assert qiskit_feature >= 0 and qiskit_feature <= 1
-    assert cirq_feature >= 0 and cirq_feature <= 1
+    feature = sm.features.compute_parallelism(CIRCUIT)
+    assert feature >= 0 and feature <= 1
 
 
 def test_compute_measurement() -> None:
-    qiskit_feature = sm.features.compute_measurement(qiskit_circuit)
-    cirq_feature = sm.features.compute_measurement(cirq_circuit)
-    assert qiskit_feature >= 0 and qiskit_feature <= 1
-    assert cirq_feature >= 0 and cirq_feature <= 1
+    feature = sm.features.compute_measurement(CIRCUIT)
+    assert feature >= 0 and feature <= 1
 
 
 def test_compute_entanglement() -> None:
-    qiskit_feature = sm.features.compute_entanglement(qiskit_circuit)
-    cirq_feature = sm.features.compute_entanglement(cirq_circuit)
-    assert qiskit_feature >= 0 and qiskit_feature <= 1
-    assert cirq_feature >= 0 and cirq_feature <= 1
+    feature = sm.features.compute_entanglement(CIRCUIT)
+    assert feature >= 0 and feature <= 1
 
 
 def test_compute_depth() -> None:
-    qiskit_feature = sm.features.compute_depth(qiskit_circuit)
-    cirq_feature = sm.features.compute_depth(cirq_circuit)
-    assert qiskit_feature >= 0 and qiskit_feature <= 1
-    assert cirq_feature >= 0 and cirq_feature <= 1
-
     qubits = cirq.LineQubit.range(4)
     test_circuit = cirq.Circuit(
         cirq.CX(qubits[0], qubits[1]),
