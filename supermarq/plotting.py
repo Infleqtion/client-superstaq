8839e7e45bcec4b47fbeafaa2bd41f950857602f
Teague Tomesh <ttomesh@princeton.edu>
2022-06-18 16:26:29 -0400

    Add plotting functions to visualize results and correlations

--- supermarq/plotting.py
+++ supermarq/plotting.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -6,6 +6,83 @@ import numpy as np
 from matplotlib.patches import Circle
 from matplotlib.projections import register_projection
 from matplotlib.projections.polar import PolarAxes
+from sklearn.linear_model import LinearRegression
+
+
+def plot_results(
+    scores: List[float], tick_labels: List[str], savefn: Optional[str] = None, show: bool = True
+) -> None:
+    """Plot a simple bar chart of the benchmark results."""
+    _, ax = plt.subplots(dpi=150)
+
+    width = 0.4
+
+    xvals = np.arange(len(scores))
+
+    ax.bar(xvals, scores, width=width, tick_label=tick_labels, align="center")
+
+    ax.set_xticks(range(len(scores)))
+    ax.set_ylabel("Score")
+    plt.tight_layout()
+
+    if savefn is not None:
+        # Don't want to save figures when running tests
+        plt.savefig(savefn)  # pragma: no cover
+
+    if show:
+        # Tests will hang if we show figures during tests
+        plt.show()  # pragma: no cover
+
+    plt.close()
+
+
+def plot_correlations(
+    benchmark_features: Dict[str, List[float]],
+    device_scores: Dict[str, float],
+    feature_labels: List,
+    device_name: str,
+    savefn: Optional[str] = None,
+    show: bool = True,
+) -> None:
+
+    temp_correlations = []
+    for i in range(len(feature_labels)):
+        x, y = [], []
+        for benchmark in device_scores.keys():
+            x.append(benchmark_features[benchmark][i])
+            y.append(device_scores[benchmark])
+
+        proper_x = np.array(x)[:, np.newaxis]
+        proper_y = np.array(y)
+        model = LinearRegression().fit(proper_x, proper_y)
+        correlation = model.score(proper_x, proper_y)
+        temp_correlations.append(correlation)
+
+    correlations = np.array([temp_correlations])
+
+    _, ax = plt.subplots(dpi=300)
+    im, _ = heatmap(
+        correlations,
+        ax,
+        [device_name],
+        feature_labels,
+        cmap="cool",
+        vmin=0,
+        cbarlabel=r"Coefficient of Determination, $R^2$",
+        cbar_kw={"pad": 0.01},
+    )
+
+    annotate_heatmap(im, size=7)
+
+    plt.tight_layout()
+    if savefn is not None:
+        # Don't want to save figures when running tests
+        plt.savefig(savefn)  # pragma: no cover
+
+    if show:
+        # Tests will hang if we show figures during tests
+        plt.show()  # pragma: no cover
+    plt.close()
 
 
 def plot_benchmark(
@@ -60,6 +137,138 @@ def plot_benchmark(
         plt.show()  # pragma: no cover
 
     plt.close()
+
+
+def heatmap(
+    data: np.ndarray,
+    ax: matplotlib.axes.Axes,
+    row_labels: List[str],
+    col_labels: List[str],
+    cbar_kw: Optional[Dict] = None,
+    cbarlabel: str = "",
+    **kwargs: Any
+) -> Tuple[Any, Any]:
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (N, M).
+    row_labels
+        A list or array of length N with the labels for the rows.
+    col_labels
+        A list or array of length M with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    if cbar_kw is None:
+        cbar_kw = {}  # pragma: no cover
+    cbar = ax.figure.colorbar(im, ax=ax, orientation="horizontal", **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom", fontsize=8)
+    cbar.ax.tick_params(labelsize=10)
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels, fontsize=8)  # regular fontsize is 12
+    ax.set_yticklabels(row_labels, fontsize=7)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right", rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1] + 1) - 0.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0] + 1) - 0.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle="-", linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    # Rotate the 3 typical features separately
+    for t in ax.get_xticklabels()[-3:]:
+        t.set_horizontalalignment("left")
+        t.set_rotation(35)
+
+    return im, cbar
+
+
+def annotate_heatmap(
+    im: matplotlib.image,
+    data: Optional[np.ndarray] = None,
+    valfmt: Any = "{x:.2f}",
+    textcolors: Tuple[str, str] = ("black", "white"),
+    threshold: Optional[float] = None,
+    **textkw: Any
+) -> List:
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)  # pragma: no cover
+    else:
+        threshold = im.norm(data.max()) / 2.0
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center", verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
 
 
 def radar_factory(num_vars: int) -> np.ndarray:
