020f1cbd92dbf475c2f4ecbe225f18a48e97de89
Michael A. Perlin <mika.perlin@gmail.com>
2022-07-18 13:43:35 -0500

    updates in light of check script changes (#40)
    * updates in light of check script changes
    
    * change supermarq[dev_env] to supermarq[dev]
    
    * require app-ss[dev] in dev-requirements.txt
    
    * minor fixes

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import io
-import os
 
 from setuptools import find_packages, setup
 
@@ -14,13 +13,12 @@ description = "SupermarQ is a scalable, application-centric quantum benchmarking
 # README file as long_description.
 long_description = io.open("README.md", encoding="utf-8").read()
 
-
 # Read in requirements
-requirements = open(os.path.dirname(os.path.realpath(__file__)) + "/requirements.txt").readlines()
+requirements = open("requirements.txt").readlines()
 requirements = [r.strip() for r in requirements]
-dev_requirements = open(
-    os.path.dirname(os.path.realpath(__file__)) + "/dev-requirements.txt"
-).readlines()
+
+# Read in dev requirements, installed with 'pip install supermarq[dev]'
+dev_requirements = open("dev-requirements.txt").readlines()
 dev_requirements = [r.strip() for r in dev_requirements]
 
 supermarq_packages = ["supermarq"] + [
@@ -38,9 +36,7 @@ setup(
     author_email="pranav@super.tech",
     python_requires=(">=3.8.0"),
     install_requires=requirements,
-    extras_require={
-        "dev_env": dev_requirements,
-    },
+    extras_require={"dev": dev_requirements},
     license="Apache 2",
     description=description,
     long_description=long_description,
