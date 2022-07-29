d6df4dd1b13c086c2c515540983e715a113a65f9
Stephanie Lee <72408904+stephanielee9@users.noreply.github.com>
2022-07-28 19:03:25 -0500

    Rename `applications_superstaq` to `general_superstaq` (#41)
    * renamed check files and added DS_Store to gitignore
    
    * delete last new line in gitignore
    
    * update css and qss version and also pinned cirq to 0.15.0

--- check/all_.py
+++ check/all_.py
@@ -2,7 +2,7 @@
 
 import sys
 
-import applications_superstaq.check
+import general_superstaq.check
 
 if __name__ == "__main__":
-    exit(applications_superstaq.check.all_.run(*sys.argv[1:]))
+    exit(general_superstaq.check.all_.run(*sys.argv[1:]))
