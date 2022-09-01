39faefda434bb7f7891b6dd81f4fb8a9f2bdafe3
Pranav Gokhale <pranav@super.tech>
2022-08-31 20:27:46 -0500

    support py3.7 (#46)
    * support py3.7
    
    * update
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * update
    
    * reorder
    
    * bump minor version

--- supermarq/simulation.py
+++ supermarq/simulation.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def get_ideal_counts(circuit: cirq.Circuit) -> collections.Counter:
     ideal_counts = {}
-    for i, amplitude in enumerate(circuit.final_state_vector()):
+    for i, amplitude in enumerate(circuit.final_state_vector(ignore_terminal_measurements=True)):
         bitstring = f"{i:>0{len(circuit.all_qubits())}b}"
         probability = np.abs(amplitude) ** 2
         ideal_counts[bitstring] = probability
