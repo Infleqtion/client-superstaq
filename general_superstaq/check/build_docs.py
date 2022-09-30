d7374d61f0264cc4ba141687d8bf4fc0af3d5f79
Michael A. Perlin <mika.perlin@gmail.com>
2022-09-30 18:53:42 -0500

    don't build docs if there is no `docs/Makefile` (#162)
    fixes #161

--- general_superstaq/check/build_docs.py
+++ general_superstaq/check/build_docs.py
@@ -22,7 +22,8 @@ def run(*args: str, sphinx_paths: Optional[List[str]] = None) -> int:
     parser.parse_args(args)  # placeholder parsing to enable printing help text
 
     docs_dir = os.path.join(check_utils.root_dir, "docs")
-    if os.path.isdir(docs_dir):
+    make_file = os.path.join(docs_dir, "Makefile")
+    if os.path.isfile(make_file):
         if sphinx_paths:
             for path in sphinx_paths:
                 subprocess.run(
