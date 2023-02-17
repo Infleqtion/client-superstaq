f2f154bfdb377d00ac9b8d2e7f834a2a8e949812
richrines1 <85512171+richrines1@users.noreply.github.com>
2023-02-17 15:47:20 -0600

    clean up in anticipation of repo merge (#445)

--- check/pytest_.py
+++ check/pytest_.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-from typing import List, Optional
 
 import general_superstaq.check
 
 if __name__ == "__main__":
-
-    if "--notebook" in sys.argv[1:]:
-        EXCLUDE: Optional[List[str]] = [
-            "examples/aqt.ipynb",
-        ]
-    else:
-        EXCLUDE = None
-
-    exit(general_superstaq.check.pytest_.run(*sys.argv[1:], exclude=EXCLUDE))
+    args = sys.argv[1:]
+    args += ["-x", "examples/aqt.ipynb"]
+    exit(general_superstaq.check.pytest_.run(*args))
