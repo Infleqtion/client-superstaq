30c5ff5af2827115dc9d14ab353d0440f4a31316
Teague Tomesh <ttomesh@princeton.edu>
2023-03-06 12:52:24 -0600

    Add docstrings to features

--- supermarq/features.py
+++ supermarq/features.py
@@ -3,33 +3,63 @@ import cirq
 import supermarq as sm
 
 
-def compute_communication(  # pylint: disable=missing-function-docstring
+def compute_communication(
     circuit: cirq.Circuit,
 ) -> float:
+    """Compute the *communication* feature of the input circuit.
+
+    This function acts a wrapper which first converts the input `cirq.Circuit`
+    into a `qiskit.QuantumCircuit` before calculating the feature value.
+    """
     return sm.converters.compute_communication_with_qiskit(sm.converters.cirq_to_qiskit(circuit))
 
 
-def compute_liveness(circuit: cirq.Circuit) -> float:  # pylint: disable=missing-function-docstring
+def compute_liveness(circuit: cirq.Circuit) -> float:
+    """Compute the *liveness* feature of the input circuit.
+
+    This function acts a wrapper which first converts the input `cirq.Circuit`
+    into a `qiskit.QuantumCircuit` before calculating the feature value.
+    """
     return sm.converters.compute_liveness_with_qiskit((sm.converters.cirq_to_qiskit(circuit)))
 
 
-def compute_parallelism(  # pylint: disable=missing-function-docstring
+def compute_parallelism(
     circuit: cirq.Circuit,
 ) -> float:
+    """Compute the *parallelism* feature of the input circuit.
+
+    This function acts a wrapper which first converts the input `cirq.Circuit`
+    into a `qiskit.QuantumCircuit` before calculating the feature value.
+    """
     return sm.converters.compute_parallelism_with_qiskit(sm.converters.cirq_to_qiskit(circuit))
 
 
-def compute_measurement(  # pylint: disable=missing-function-docstring
+def compute_measurement(
     circuit: cirq.Circuit,
 ) -> float:
+    """Compute the *measurement* feature of the input circuit.
+
+    This function acts a wrapper which first converts the input `cirq.Circuit`
+    into a `qiskit.QuantumCircuit` before calculating the feature value.
+    """
     return sm.converters.compute_measurement_with_qiskit(sm.converters.cirq_to_qiskit(circuit))
 
 
-def compute_entanglement(  # pylint: disable=missing-function-docstring
+def compute_entanglement(
     circuit: cirq.Circuit,
 ) -> float:
+    """Compute the *entanglement* feature of the input circuit.
+
+    This function acts a wrapper which first converts the input `cirq.Circuit`
+    into a `qiskit.QuantumCircuit` before calculating the feature value.
+    """
     return sm.converters.compute_entanglement_with_qiskit(sm.converters.cirq_to_qiskit(circuit))
 
 
-def compute_depth(circuit: cirq.Circuit) -> float:  # pylint: disable=missing-function-docstring
+def compute_depth(circuit: cirq.Circuit) -> float:
+    """Compute the *depth* feature of the input circuit.
+
+    This function acts a wrapper which first converts the input `cirq.Circuit`
+    into a `qiskit.QuantumCircuit` before calculating the feature value.
+    """
     return sm.converters.compute_depth_with_qiskit(sm.converters.cirq_to_qiskit(circuit))
