50dac3583dd7484b9a38f3339828abd282634d42
Teague Tomesh <ttomesh@princeton.edu>
2023-03-13 17:50:28 -0500

    DTC unit tests

---
+++ supermarq/benchmarks/time_crystal_evolution_test.py
@@ -0,0 +1,15 @@
+# pylint: disable=missing-function-docstring
+import supermarq
+from supermarq.benchmarks.time_crystal_evolution import TimeCrystalEvolution
+
+
+def test_time_crystal_circuit() -> None:
+    dtc = TimeCrystalEvolution(4, 1)
+    assert len(dtc.circuit().all_qubits()) == 4
+
+
+def test_time_crystal_score() -> None:
+    dtc = TimeCrystalEvolution(4, 1)
+    assert dtc._average_magnetization({"1111": 1}, 1) == -1.0
+    assert dtc._average_magnetization({"0000": 1}, 1) == 1.0
+    assert dtc.score(supermarq.simulation.get_ideal_counts(dtc.circuit())) > 0.99
