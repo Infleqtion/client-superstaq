9cae9a2981a96d7dc39682d43dd4cec31f4aaef9
rich <rich@super.tech>
2023-03-14 17:09:31 -0500

    fix comment

--- dev_tools/update-pypi-version.sh
+++ dev_tools/update-pypi-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This publishes general-superstaq to PyPI.
+# This publishes supermarq to PyPI.
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 toplevel=$(git rev-parse --show-toplevel --path-format=absolute)
 
