d563dec4e7894d863c93c99cc6561d1e48ac73e1
richrines1 <85512171+richrines1@users.noreply.github.com>
2023-03-14 18:03:45 -0500

    move qss to subdir (#318)
    preparation for repo merge: moves everything that's unique to
    qiskit-superstaq into a single subdirectory
    
    related: https://github.com/SupertechLabs/general-superstaq/pull/215 and
    https://github.com/SupertechLabs/cirq-superstaq/pull/442
    
    this should be the final pr merged into this repo.

--- dev_tools/update-pypi-version.sh
+++ dev_tools/update-pypi-version.sh
@@ -2,6 +2,8 @@
 
 # This publishes qiskit-superstaq to PyPI.
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$(git rev-parse --show-toplevel)"
+toplevel=$(git rev-parse --show-toplevel --path-format=absolute)
+
+cd "$toplevel/qiskit-superstaq"
 python setup.py bdist_wheel
 twine upload dist/* -u __token__ -p "$PYPI_API_KEY"
