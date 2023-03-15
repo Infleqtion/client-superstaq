e79837a73643c00751ad449387267845b3ff11ca
rich <rich@super.tech>
2023-03-14 21:32:18 -0500

    resolve merge conflicts

--- dev_tools/update-pypi-version.sh
+++ dev_tools/update-pypi-version.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-# This publishes general-superstaq to PyPI.
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 toplevel=$(git rev-parse --show-toplevel --path-format=absolute)
 
+# This publishes general-superstaq to PyPI.
 cd "$toplevel/general-superstaq"
+python setup.py bdist_wheel
+twine upload dist/* -u __token__ -p "$PYPI_API_KEY"
+
+# This publishes qiskit-superstaq to PyPI.
+cd "$toplevel/qiskit-superstaq"
+python setup.py bdist_wheel
+twine upload dist/* -u __token__ -p "$PYPI_API_KEY"
+
+# This publishes cirq-superstaq to PyPI.
+cd "$toplevel/cirq-superstaq"
+python setup.py bdist_wheel
+twine upload dist/* -u __token__ -p "$PYPI_API_KEY"
+
+# This publishes supermarq to PyPI.
+cd "$toplevel/superstaq-benchmarq"
 python setup.py bdist_wheel
 twine upload dist/* -u __token__ -p "$PYPI_API_KEY"
