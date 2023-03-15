fc354466f2a7e250aeef8bd72f9202942c6d50ec
richrines1 <85512171+richrines1@users.noreply.github.com>
2023-03-14 20:50:47 -0600

    Revert "merge repos"

--- check/pytest_.py
+++ check/pytest_.py
@@ -5,9 +5,4 @@ import sys
 import general_superstaq.check
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    args += ["-x", "cirq-superstaq/examples/aqt.ipynb"]
-    args += ["-x", "qiskit-superstaq/examples/aqt.ipynb"]
-    args += ["-x", "qiskit-superstaq/examples/uchicago_workshop.ipynb"]
-    args += ["-x", "docs/*"]
-    exit(general_superstaq.check.pytest_.run(*args))
+    exit(general_superstaq.check.pytest_.run(*sys.argv[1:]))
