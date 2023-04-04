9e38b208f4a704ef2651c8362711345fd8b0c913
richrines1 <85512171+richrines1@users.noreply.github.com>
2023-04-04 11:58:30 -0500

    mock less in `qss.superstaq_backend_test` (#429)

--- qiskit-superstaq/qiskit_superstaq/superstaq_backend_test.py
+++ qiskit-superstaq/qiskit_superstaq/superstaq_backend_test.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-function-docstring,missing-class-docstring
-from unittest.mock import MagicMock
+from unittest import mock
 
 import general_superstaq as gss
 import pytest
@@ -9,25 +9,14 @@ import qiskit_superstaq as qss
 
 
 def test_default_options() -> None:
-    ss_provider = qss.SuperstaQProvider(api_key="MY_TOKEN")
-    device = qss.SuperstaQBackend(
-        provider=ss_provider,
+    provider = qss.SuperstaQProvider(api_key="MY_TOKEN")
+    backend = qss.SuperstaQBackend(
+        provider=provider,
         remote_host=gss.API_URL,
         target="ibmq_qasm_simulator",
     )
 
-    assert qiskit.providers.Options(shots=1000) == device._default_options()
-
-
-class MockProvider(qss.SuperstaQProvider):
-    def __init__(self) -> None:
-        self.api_key = "super.tech"
-
-
-class MockDevice(qss.SuperstaQBackend):
-    def __init__(self) -> None:
-        super().__init__(MockProvider(), "super.tech", "ss_example_qpu")
-        self._provider = MockProvider()
+    assert qiskit.providers.Options(shots=1000) == backend._default_options()
 
 
 def test_validate_target() -> None:
@@ -49,28 +38,23 @@ def test_run() -> None:
     qc.h(0)
     qc.cx(0, 1)
     qc.measure([0, 0], [1, 1])
-    device = MockDevice()
 
-    mock_client = MagicMock()
-    mock_client.create_job.return_value = {
-        "job_ids": ["job_id"],
-        "status": "ready",
-    }
+    backend = qss.SuperstaQProvider(api_key="123").get_backend("ss_example_qpu")
 
-    device._provider._client = mock_client
-
-    answer = device.run(circuits=qc, shots=1000)
-    expected = qss.SuperstaQJob(device, "job_id")
-    assert answer == expected
+    with mock.patch(
+        "general_superstaq.superstaq_client._SuperstaQClient.create_job",
+        return_value={"job_ids": ["job_id"], "status": "ready"},
+    ):
+        answer = backend.run(circuits=qc, shots=1000)
+        expected = qss.SuperstaQJob(backend, "job_id")
+        assert answer == expected
 
     with pytest.raises(ValueError, match="Circuit has no measurements to sample"):
         qc.remove_final_measurements()
-        device.run(qc, shots=1000)
+        backend.run(qc, shots=1000)
 
 
 def test_multi_circuit_run() -> None:
-    device = MockDevice()
-
     qc1 = qiskit.QuantumCircuit(1, 1)
     qc1.h(0)
     qc1.measure(0, 0)
@@ -80,17 +64,15 @@ def test_multi_circuit_run() -> None:
     qc2.cx(0, 1)
     qc2.measure([0, 1], [0, 1])
 
-    mock_client = MagicMock()
-    mock_client.create_job.return_value = {
-        "job_ids": ["job_id"],
-        "status": "ready",
-    }
-    device._provider._client = mock_client
+    backend = qss.SuperstaQProvider(api_key="123").get_backend("ss_example_qpu")
 
-    answer = device.run(circuits=[qc1, qc2], shots=1000)
-    expected = qss.SuperstaQJob(device, "job_id")
-
-    assert answer == expected
+    with mock.patch(
+        "general_superstaq.superstaq_client._SuperstaQClient.create_job",
+        return_value={"job_ids": ["job_id"], "status": "ready"},
+    ):
+        answer = backend.run(circuits=[qc1, qc2], shots=1000)
+        expected = qss.SuperstaQJob(backend, "job_id")
+        assert answer == expected
 
 
 def test_multi_arg_run() -> None:
@@ -98,36 +80,26 @@ def test_multi_arg_run() -> None:
     qc.h(0)
     qc.cx(0, 1)
     qc.measure([0, 0], [1, 1])
-    device = MockDevice()
 
-    mock_client = MagicMock()
-    mock_client.create_job.return_value = {
-        "job_ids": ["job_id"],
-        "status": "ready",
-    }
+    backend = qss.SuperstaQProvider(api_key="123").get_backend("ss_example_qpu")
 
-    device._provider._client = mock_client
-
-    answer = device.run(circuits=qc, shots=1000, method="fake_method", options={"test": "123"})
-    expected = qss.SuperstaQJob(device, "job_id")
-    assert answer == expected
+    with mock.patch(
+        "general_superstaq.superstaq_client._SuperstaQClient.create_job",
+        return_value={"job_ids": ["job_id"], "status": "ready"},
+    ):
+        answer = backend.run(circuits=qc, shots=1000, method="fake_method", options={"test": "123"})
+        expected = qss.SuperstaQJob(backend, "job_id")
+        assert answer == expected
 
 
 def test_eq() -> None:
-
-    assert MockDevice() != 3
-
     provider = qss.SuperstaQProvider(api_key="123")
 
-    backend1 = qss.SuperstaQBackend(
-        provider=provider, remote_host=gss.API_URL, target="ibmq_qasm_simulator"
-    )
-    backend2 = qss.SuperstaQBackend(
-        provider=provider, remote_host=gss.API_URL, target="ibmq_athens_qpu"
-    )
+    backend1 = provider.get_backend("ibmq_qasm_simulator")
+    assert backend1 != 3
+
+    backend2 = provider.get_backend("ibmq_athens_qpu")
     assert backend1 != backend2
 
-    backend3 = qss.SuperstaQBackend(
-        provider=provider, remote_host=gss.API_URL, target="ibmq_qasm_simulator"
-    )
+    backend3 = provider.get_backend("ibmq_qasm_simulator")
     assert backend1 == backend3
