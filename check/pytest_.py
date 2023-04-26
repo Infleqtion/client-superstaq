3e2d64323bf01e771f302d34a2628d26b8a524d7
Stephanie <stephanie.lee929@gmail.com>
2023-04-26 14:21:22 -0500

    updated pytest to ignore certain subdirectories in docs

--- check/pytest_.py
+++ check/pytest_.py
@@ -6,8 +6,8 @@ import general_superstaq.check
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    args += ["-x", "cirq-superstaq/examples/aqt.ipynb"]
-    args += ["-x", "qiskit-superstaq/examples/aqt.ipynb"]
     args += ["-x", "qiskit-superstaq/examples/uchicago_workshop.ipynb"]
-    args += ["-x", "docs/*"]
+    args += ["-x", "docs/source/apps/*"]
+    args += ["-x", "docs/source/get_started/*"]
+    args += ["-x", "docs/source/optimizations/aqt/*"]
     exit(general_superstaq.check.pytest_.run(*args))
