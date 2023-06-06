157202bf1508ed4a8b81bf2f1beefbce36f10f3c
Victory Omole <vtomole2@gmail.com>
2023-06-06 11:30:30 -0500

    Update locustfile.py

--- dev_tools/locustfile.py
+++ dev_tools/locustfile.py
@@ -24,12 +24,6 @@ def _(environment: Environment) -> None:
     if environment.stats.total.fail_ratio > 0.01:
         logging.error("Test failed due to failure ratio > 1%")
         environment.process_exit_code = 1
-    elif environment.stats.total.avg_response_time > 200:
-        logging.error("Test failed due to average response time ratio > 200 ms")
-        environment.process_exit_code = 1
-    elif environment.stats.total.get_response_time_percentile(0.95) > 800:
-        logging.error("Test failed due to 95th percentile response time > 800 ms")
-        environment.process_exit_code = 1
     else:
         environment.process_exit_code = 0
 
