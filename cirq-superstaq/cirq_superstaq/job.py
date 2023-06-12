451148c9f32d1a0f71ba9d37f1868c5e8f6b491d
rich <rich@super.tech>
2023-06-12 18:09:04 -0500

    css.Job docstrings

--- cirq-superstaq/cirq_superstaq/job.py
+++ cirq-superstaq/cirq_superstaq/job.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Represents a job created via the SuperstaQ API."""
+"""Represents a job created via the Superstaq API."""
 import time
 from typing import Any, Dict, Tuple
 
@@ -22,35 +22,34 @@ from cirq._doc import document
 
 @cirq.value_equality(unhashable=True)
 class Job:
-    """A job created on the SuperstaQ API.
+    """A job created on the Superstaq API.
 
-    Note that this is mutable, when calls to get status or results are made
-    the job updates itself to the results returned from the API.
+    Note that this is mutable, when calls to get status or results are made the job updates itself
+    to the results returned from the API.
 
-    If a job is canceled or deleted, only the job id and the status remain
-    valid.
+    If a job is canceled or deleted, only the job id and the status remain valid.
     """
 
     TERMINAL_STATES = ("Done", "Canceled", "Failed", "Deleted")
     document(
         TERMINAL_STATES,
-        "States of the SuperstaQ API job from which the job cannot transition. "
+        "States of the Superstaq API job from which the job cannot transition. "
         "Note that deleted can only exist in a return call from a delete "
         "(subsequent calls will return not found).",
     )
 
     NON_TERMINAL_STATES = ("Ready", "Submitted", "Running")
     document(
-        NON_TERMINAL_STATES, "States of the SuperstaQ API job which can transition to other states."
+        NON_TERMINAL_STATES, "States of the Superstaq API job which can transition to other states."
     )
 
     ALL_STATES = TERMINAL_STATES + NON_TERMINAL_STATES
-    document(ALL_STATES, "All states that an SuperstaQ API job can exist in.")
+    document(ALL_STATES, "All states that an Superstaq API job can exist in.")
 
     UNSUCCESSFUL_STATES = ("Canceled", "Failed", "Deleted")
     document(
         UNSUCCESSFUL_STATES,
-        "States of the SuperstaQ API job when it was not successful and so does not have any "
+        "States of the Superstaq API job when it was not successful and so does not have any "
         "data associated with it beyond an id and a status.",
     )
 
@@ -62,7 +61,7 @@ class Job:
 
         Args:
             client: The client used for calling the API.
-            job_id: unique identifier for the job.
+            job_id: Unique identifier for the job.
         """
         self._client = client
         self._job: Dict[str, Any] = {"status": "Submitted"}
@@ -75,24 +74,23 @@ class Job:
 
     def _check_if_unsuccessful(self) -> None:
         if self.status() in self.UNSUCCESSFUL_STATES:
-            raise gss.superstaq_exceptions.SuperstaQUnsuccessfulJobException(
-                self.job_id(), self.status()
-            )
+            raise gss.SuperstaQUnsuccessfulJobException(self.job_id(), self.status())
 
     def job_id(self) -> str:
-        """Returns the job id (UID) for the job.
+        """Gets the job id of this job.
 
-        This is the id used for identifying the job by the API.
+        This is the unique identifier used for identifying the job by the API.
+
+        Returns:
+            This job's id (a string).
         """
         return self._job_id
 
     def status(self) -> str:
         """Gets the current status of the job.
 
-        If the current job is in a non-terminal state,
-        this will update the job and return the current status.
-        A full list of states is given in
-        `cirq_superstaq.Job.ALL_STATES`.
+        If the current job is in a non-terminal state, this will update the job and return the
+        current status. A full list of states is given in `cirq_superstaq.Job.ALL_STATES`.
 
         Raises:
             SuperstaQException: If the API is not able to get the status of the job.
@@ -104,52 +102,56 @@ class Job:
         return self._job["status"]
 
     def target(self) -> str:
-        """Returns the target where the job is to be run, or was run.
+        """Gets the Superstaq target associated with this job.
 
         Returns:
-            'qpu' or 'simulator' depending on where the job was run or is running.
+            The target to which this job was submitted.
 
         Raises:
-            SuperstaQUnsuccessfulJob: If the job has failed, been canceled, or deleted.
+            SuperstaQUnsuccessfulJobException: If the job has failed, been canceled, or deleted.
             SuperstaQException: If unable to get the status of the job from the API.
         """
         self._check_if_unsuccessful()
         return self._job["target"]
 
     def num_qubits(self) -> int:
-        """Returns the number of qubits for the job.
+        """Gets the number of qubits required for this job.
+
+        Returns:
+            The number of qubits used in this job.
 
         Raises:
-            SuperstaQUnsuccessfulJob: If the job has failed, been canceled, or deleted.
+            SuperstaQUnsuccessfulJobException: If the job has failed, been canceled, or deleted.
             SuperstaQException: If unable to get the status of the job from the API.
         """
         self._check_if_unsuccessful()
         return self._job["num_qubits"]
 
     def repetitions(self) -> int:
-        """Returns the number of repetitions for the job.
+        """Gets the number of repetitions requested for this job.
+
+        Returns:
+            The number of repetitions for this job.
 
         Raises:
-            SuperstaQUnsuccessfulJob: If the job has failed, been canceled, or deleted.
+            SuperstaQUnsuccessfulJobException: If the job has failed, been canceled, or deleted.
             SuperstaQException: If unable to get the status of the job from the API.
         """
         self._check_if_unsuccessful()
         return self._job["shots"]
 
-    def counts(  # pylint: disable=missing-raises-doc
-        self, timeout_seconds: int = 7200, polling_seconds: float = 1.0
-    ) -> Dict[str, int]:
-        """Polls the SuperstaQ API for results.
+    def counts(self, timeout_seconds: int = 7200, polling_seconds: float = 1.0) -> Dict[str, int]:
+        """Polls the Superstaq API for results.
 
         Args:
             timeout_seconds: The total number of seconds to poll for.
             polling_seconds: The interval with which to poll.
 
         Returns:
-            collections.Counter that represents the results of the measurements
+            A dictionary containing the results of the measurements.
 
         Raises:
-            SuperstaQUnsuccessfulJob: If the job has failed, been canceled, or deleted.
+            SuperstaQUnsuccessfulJobException: If the job has failed, been canceled, or deleted.
             SuperstaQException: If unable to get the results from the API.
         """
         time_waited_seconds: float = 0.0
@@ -159,13 +161,15 @@ class Job:
                 break
             time.sleep(polling_seconds)
             time_waited_seconds += polling_seconds
+
         if self.status() != "Done":
+            status = self.status()
             if "failure" in self._job and "error" in self._job["failure"]:
+                # if possible append a message to the failure status, e.g. "Failed (<message>)"
                 error = self._job["failure"]["error"]
-                raise RuntimeError(f"Job failed. Error message: {error}")
-            raise RuntimeError(
-                f"Job was not completed successfully. Instead had status: {self.status()}"
-            )
+                status += f" ({error})"
+            raise gss.SuperstaQUnsuccessfulJobException(self._job_id, status)
+
         return self._job["samples"]
 
     def __str__(self) -> str:
