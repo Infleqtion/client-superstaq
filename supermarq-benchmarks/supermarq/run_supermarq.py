3f935e7fb458850ffdb32c9f211267fd62c05c47
Emilio <epelaez@uchicago.edu>
2023-06-21 10:56:04 -0500

    fix type checking

--- supermarq-benchmarks/supermarq/run_supermarq.py
+++ supermarq-benchmarks/supermarq/run_supermarq.py
@@ -1,7 +1,8 @@
-from typing import Dict, List, Tuple
 from datetime import datetime
+from typing import Dict, List, Tuple
 
 import cirq_superstaq as css
+
 import supermarq
 
 BENCHMARKS: List[Tuple[supermarq.benchmark.Benchmark, str]] = [
@@ -11,13 +12,27 @@ BENCHMARKS: List[Tuple[supermarq.benchmark.Benchmark, str]] = [
     (supermarq.bit_code.BitCode(3, 3, [1, 0, 1]), "bitcode3"),
 ]
 
-def get_qpu_targets(targets: Dict[str, str]) -> List[str]:
-    qpu_targets = []
+
+def get_qpu_targets(targets: Dict[str, List[str]]) -> List[str]:
+    """Get real device targets.
+
+    Args:
+        targets: Output from `service.get_targets()`.
+
+    Returns:
+        A list with the targets corresponding to real devices.
+    """
+    qpu_targets: List[str] = []
     run_targets = targets.get("compile-and-run")
+
+    if run_targets is None:
+        return qpu_targets
+
     for t in run_targets:
         if t.endswith("qpu"):
             qpu_targets.append(t)
     return qpu_targets
+
 
 service = css.Service()
 targets = service.get_targets()
@@ -25,16 +40,19 @@ qpu_targets = get_qpu_targets(targets)
 
 for target in qpu_targets:
     for benchmark, label in BENCHMARKS:
-        date = datetime.today().strftime('%Y-%m-%d')
+        date = datetime.today().strftime("%Y-%m-%d")
         tag = f"{date}-{label}-{target}"
         print(tag)
 
-        # Since there is no way to determine if a target supports mid-circuit measurement purely 
+        # Since there is no way to determine if a target supports mid-circuit measurement purely
         # through `service.target_info()`, we use a `try` block to catch when we submit an invalid
-        # circuit to a device that does not support it. 
+        # circuit to a device that does not support it.
         try:
             job = service.create_job(
-                benchmark.circuit(), repetitions=1000, target=target, options={"tag": tag, "qiskit_pulse": False}
+                benchmark.circuit(),
+                repetitions=1000,
+                target=target,
+                options={"tag": tag, "qiskit_pulse": False},
             )
         except Exception:
             pass
