15bc29b912e0a5c223fe925780bce3c8d58db86c
Michael A. Perlin <mika.perlin@gmail.com>
2023-06-22 22:03:52 -0500

    testing importlib fix

--- general-superstaq/general_superstaq/_version.py
+++ general-superstaq/general_superstaq/_version.py
@@ -1,3 +1,6 @@
-import importlib.metadata
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
 
-__version__ = importlib.metadata.version("general_superstaq")
+__version__ = importlib_metadata.version(__name__)
