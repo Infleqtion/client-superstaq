65b7842b0228af885b4fd869fbce7549bcbb80e4
Victory Omole <vtomole2@gmail.com>
2023-06-23 16:55:51 -0500

    Update cirq-superstaq/cirq_superstaq/ops/qubit_gates.py
    Co-authored-by: dowusu-antwi <david@super.tech>

--- cirq-superstaq/cirq_superstaq/ops/qubit_gates.py
+++ cirq-superstaq/cirq_superstaq/ops/qubit_gates.py
@@ -562,7 +562,9 @@ class ParallelGates(cirq.Gate, cirq.InterchangeableQubitsGate):
 
 
 def parallel_gates_operation(*ops: cirq.Operation) -> cirq.Operation:
-    """Given operations acting on disjoint qubits, constructs a single css.ParallelGates instance
+    """Constructs a parallel gates operation.
+    
+    Given operations acting on disjoint qubits, constructs a single css.ParallelGates instance
     and applies it such that each operation's .gate is applied to its .qubits.
 
     Args:
