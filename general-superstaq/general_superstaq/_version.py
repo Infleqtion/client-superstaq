b437a1c50102291caecbf8ec798dec391ad7e8e7
Michael A. Perlin <mika.perlin@gmail.com>
2023-06-22 22:09:33 -0500

    final(?) importlib fix

--- general-superstaq/general_superstaq/_version.py
+++ general-superstaq/general_superstaq/_version.py
@@ -1,6 +1,6 @@
 try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
+    import importlib.metadata as importlib_metadata  # pragma: no cover
+except ModuleNotFoundError:  # pragma: no cover
+    import importlib_metadata  # pragma: no cover
 
 __version__ = importlib_metadata.version("general_superstaq")
