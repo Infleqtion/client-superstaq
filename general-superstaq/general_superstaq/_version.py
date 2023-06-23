793180b07a82e8b5b7fcfdcf3fb41c9440311b9e
Michael A. Perlin <mika.perlin@gmail.com>
2023-06-22 22:17:01 -0500

    typing fix

--- general-superstaq/general_superstaq/_version.py
+++ general-superstaq/general_superstaq/_version.py
@@ -2,6 +2,6 @@
 try:
     import importlib.metadata as importlib_metadata  # pragma: no cover
 except ModuleNotFoundError:  # pragma: no cover
-    import importlib_metadata  # pragma: no cover
+    import importlib_metadata  # type: ignore[no-redef]  # pragma: no cover
 
 __version__ = importlib_metadata.version("general_superstaq")
