ec0891d894a69c2efb6f9c3bab7f1ab17c5d5883
Michael A. Perlin <mika.perlin@gmail.com>
2023-06-22 22:12:49 -0500

    add comment to explain weird importlib.metadata import

--- general-superstaq/general_superstaq/_version.py
+++ general-superstaq/general_superstaq/_version.py
@@ -1,3 +1,4 @@
+# Note: this try/except is necessary to support python3.7, which does not have importlib.metadata
 try:
     import importlib.metadata as importlib_metadata  # pragma: no cover
 except ModuleNotFoundError:  # pragma: no cover
