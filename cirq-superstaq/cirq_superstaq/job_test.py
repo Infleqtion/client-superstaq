df44d8ec4fb5c660415018911709a0baa0375395
Bharath <bharath.thotakura@infleqtion.com>
2023-06-26 14:16:37 -0500

    pylint after license

--- cirq-superstaq/cirq_superstaq/job_test.py
+++ cirq-superstaq/cirq_superstaq/job_test.py
@@ -1,4 +1,3 @@
-# pylint: disable=missing-function-docstring
 # Copyright 2021 The Cirq Developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# pylint: disable=missing-function-docstring
 
 from __future__ import annotations
 
