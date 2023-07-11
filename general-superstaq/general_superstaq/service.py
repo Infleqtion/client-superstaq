4a2cf9ccad549ff584acc3da5b886d8c7cae513a
dhirpalshah <86183980+dhirpalshah@users.noreply.github.com>
2023-07-11 12:35:00 -0500

    Allow users to initialize client with `client = service.Service()` (#613)
    Closes #604 .
    
    Part of the ongoing effort as mentioned in #608 to allow users to
    initialize client directly without calling private classes. Still
    discussing with @vtomole and @richrines1 about potential refactoring of
    `_SuperstaqClient`, but very likely that we won't need to do that.
    
    ---------
    
    Co-authored-by: Victory Omole <vtomole2@gmail.com>

--- general-superstaq/general_superstaq/service.py
+++ general-superstaq/general_superstaq/service.py
@@ -1,22 +1,38 @@
 import os
-from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, Union
+from typing import Any, Dict, Optional, Tuple, Union
 
 import qubovert as qv
 
-if TYPE_CHECKING:
-    from general_superstaq import superstaq_client
+import general_superstaq as gss
+from general_superstaq import superstaq_client
 
 
 class Service:
     """This class contains all the services that are used to operate Superstaq."""
 
-    def __init__(self, client: "superstaq_client._SuperstaqClient"):
+    def __init__(
+        self,
+        api_key: Optional[str] = None,
+        remote_host: Optional[str] = None,
+        default_target: Optional[str] = None,
+        api_version: str = gss.API_VERSION,
+        max_retry_seconds: int = 3600,
+        verbose: bool = False,
+    ) -> None:
         """Initializes the `Service` class.
 
         Args:
             client: The Superstaq client to use.
         """
-        self._client = client
+
+        self._client = superstaq_client._SuperstaqClient(
+            client_name="general-superstaq",
+            remote_host=remote_host,
+            api_key=api_key,
+            api_version=api_version,
+            max_retry_seconds=max_retry_seconds,
+            verbose=verbose,
+        )
 
     def get_balance(self, pretty_output: bool = True) -> Union[str, float]:
         """Get the querying user's account balance in USD.
