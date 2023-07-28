90d829197db7ae914db7925b3847b4d1026177a2
richrines1 <85512171+richrines1@users.noreply.github.com>
2023-07-28 10:11:15 -0500

    parametrize integration test (#656)
    closes #648

--- qiskit-superstaq/qiskit_superstaq/daily_integration_test.py
+++ qiskit-superstaq/qiskit_superstaq/daily_integration_test.py
@@ -232,16 +232,15 @@ def test_dfe(provider: qss.superstaq_provider.SuperstaqProvider) -> None:
     assert isinstance(result, float)
 
 
-def test_submit_to_provider_simulators(provider: qss.superstaq_provider.SuperstaqProvider) -> None:
-
+@pytest.mark.parametrize(
+    "target", ["cq_hilbert_simulator", "aws_sv1_simulator", "ibmq_qasm_simulator"]
+)
+def test_submit_to_provider_simulators(target: str, provider: qss.SuperstaqProvider) -> None:
     qc = qiskit.QuantumCircuit(2, 2)
     qc.x(0)
     qc.cx(0, 1)
     qc.measure(0, 0)
     qc.measure(1, 1)
 
-    backends = ["cq_hilbert_simulator", "aws_sv1_simulator", "ibmq_qasm_simulator"]
-
-    for backend in backends:
-        job = provider.get_backend(backend).run(qc, shots=1)
-        assert job.result().get_counts() == {"11": 1}
+    job = provider.get_backend(target).run(qc, shots=1)
+    assert job.result().get_counts() == {"11": 1}
