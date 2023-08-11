84e669cf2f0257583c0b7f804a1fd728eac4d6c9
dhirpalshah <86183980+dhirpalshah@users.noreply.github.com>
2023-08-11 16:45:19 -0400

    Delete `convert_qubo_to_model` in `qubo.py` (#690)
    In `qubo.py`, the `convert_qubo_to_model` function previously was used
    to serialize a qubo before sending it in as a request. However, after
    finishing #681, there is no need for this function anymore - removed for
    cleanliness.

--- general-superstaq/general_superstaq/qubo_test.py
+++ general-superstaq/general_superstaq/qubo_test.py
@@ -1,6 +1,4 @@
 # pylint: disable=missing-function-docstring,missing-class-docstring
-import qubovert as qv
-
 import general_superstaq as gss
 
 
@@ -11,13 +9,3 @@ def test_read_json_qubo_result() -> None:
     server_return = {"solution": gss.serialization.serialize(deserialized_return)}
 
     assert repr(gss.qubo.read_json_qubo_result(server_return)) == repr(deserialized_return)
-
-
-def test_convert_qubo_to_model() -> None:
-    example_qubo = qv.QUBO({(0,): 1.0, (1,): 1.0, (0, 1): -2.0})
-    qubo_model = [
-        {"keys": ["0"], "value": 1.0},
-        {"keys": ["1"], "value": 1.0},
-        {"keys": ["0", "1"], "value": -2.0},
-    ]
-    assert gss.qubo.convert_qubo_to_model(example_qubo) == qubo_model
