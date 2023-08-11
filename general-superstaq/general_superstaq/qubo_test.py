892ab895ebf80da907bba5395561531e052c9654
Dhirpal Shah <dhirpalshah@uchicago.edu>
2023-08-11 14:54:15 -0500

    fixed fails

--- general-superstaq/general_superstaq/qubo_test.py
+++ general-superstaq/general_superstaq/qubo_test.py
@@ -1,5 +1,4 @@
 # pylint: disable=missing-function-docstring,missing-class-docstring
-import numpy as np
 import qubovert as qv
 
 import general_superstaq as gss
@@ -9,9 +8,7 @@ def test_read_json_qubo_result() -> None:
 
     deserialized_return = [{0: 0, 1: 0, 2: 0, 3: 0, 4: 0}]
 
-    server_return = {
-        "solution": gss.serialization.serialize(deserialized_return)
-    }
+    server_return = {"solution": gss.serialization.serialize(deserialized_return)}
 
     assert repr(gss.qubo.read_json_qubo_result(server_return)) == repr(deserialized_return)
 
