0d58c9e5a3e76d787062ae833dce819cd834305e
Emilio Peláez <63567458+epelaaez@users.noreply.github.com>
2023-08-30 09:57:38 -0600

    Update qiskit-superstaq/qiskit_superstaq/daily_integration_test.py
    Co-authored-by: Victory Omole <vtomole2@gmail.com>

--- qiskit-superstaq/qiskit_superstaq/daily_integration_test.py
+++ qiskit-superstaq/qiskit_superstaq/daily_integration_test.py
@@ -237,7 +237,7 @@ def test_dfe(provider: qss.superstaq_provider.SuperstaqProvider) -> None:
 def test_aces(provider: qss.superstaq_provider.SuperstaqProvider) -> None:
     backend = provider.get_backend("ss_unconstrained_simulator")
     job_id = backend.submit_aces(
-        qubits=qiskit.QuantumRegister(1),
+        qubits=[0],
         shots=100,
         num_circuits=10,
         mirror_depth=5,
