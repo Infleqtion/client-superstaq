cd12557eb7b8bf4baa2bbfd8203ce836e153e518
rich <rich@super.tech>
2023-09-07 11:55:14 -0500

    cleanup

--- check/pytest_.py
+++ check/pytest_.py
@@ -5,5 +5,6 @@ import sys
 import general_superstaq.check
 
 if __name__ == "__main__":
-    args = [*sys.argv[1:], "--exclude", "docs/source/apps/*"]
+    args = sys.argv[1:]
+    args += ["--exclude", "docs/source/apps/*"]
     exit(general_superstaq.check.pytest_.run(*args))
