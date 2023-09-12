2aef55d64ca3b1919384ebf71c46ce4521599acd
Pranav Gokhale <pranav@super.tech>
2023-09-12 09:09:11 -0500

    skip

--- check/pytest_.py
+++ check/pytest_.py
@@ -6,5 +6,5 @@ import general_superstaq.check
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    args += ["--exclude", "docs/source/apps/*"]
+    args += ["--exclude", "docs/source/apps/*", "docs/source/optimizations/ibm/ibmq_dd.ipynb"]
     exit(general_superstaq.check.pytest_.run(*args))
