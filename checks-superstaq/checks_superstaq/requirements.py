a0de6351f2f0e1faa88185537caa03a5976dbff0
Michael A. Perlin <mika.perlin@gmail.com>
2023-09-23 12:34:27 -0500

    more helpful warning

--- checks-superstaq/checks_superstaq/requirements.py
+++ checks-superstaq/checks_superstaq/requirements.py
@@ -216,11 +216,13 @@ def _get_pypi_version(package: str, silent: bool) -> Optional[str]:
 
 def _inspect_local_version(package: str, latest_version: str) -> None:
     """Check that the package is installed with the same minor version (X.Y.*) as latest_version."""
-    installed_version = _get_local_version(package)
-    if not installed_version or installed_version.split(".")[:2] != latest_version.split(".")[:2]:
+    local_version = _get_local_version(package)
+    if not local_version or local_version.split(".")[:2] != latest_version.split(".")[:2]:
         warning = f"WARNING: locally installed {package} version is not up to date."
+        version_info = f"Local version is {local_version}, but latest version is {latest_version}."
         suggestion = f"Try calling 'python -m pip install --upgrade {package}'."
         print(check_utils.warning(warning))
+        print(check_utils.warning(version_info))
         print(check_utils.warning(suggestion))
 
 
