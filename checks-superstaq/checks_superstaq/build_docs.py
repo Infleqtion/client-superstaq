27d866160241791e0bf017404fb19204c9cf560b
Michael A. Perlin <mika.perlin@gmail.com>
2023-09-23 10:27:57 -0500

    fix check script imports of check_utils

--- checks-superstaq/checks_superstaq/build_docs.py
+++ checks-superstaq/checks_superstaq/build_docs.py
@@ -5,7 +5,7 @@ import sys
 import textwrap
 from typing import List, Optional
 
-from general_superstaq.check import check_utils
+from checks_superstaq import check_utils
 
 
 @check_utils.enable_exit_on_failure
