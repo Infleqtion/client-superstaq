9e99cb18e5bceebb17b59990e9360b90066eb01d
Michael A. Perlin <mika.perlin@gmail.com>
2023-09-23 21:59:10 -0500

    revert changes to requirements.py

--- checks-superstaq/checks_superstaq/requirements.py
+++ checks-superstaq/checks_superstaq/requirements.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import fnmatch
 import functools
-import importlib.metadata
 import json
 import os
 import re
+import subprocess
 import sys
 import textwrap
 import urllib.request
-from typing import Dict, Iterable, List, Optional, Tuple, Union
+from typing import Dict, Iterable, List, Tuple, Union
 
 import packaging.version
 
+import checks_superstaq
 from checks_superstaq import check_utils
 
 
@@ -65,11 +66,14 @@ def run(
 
     files = check_utils.extract_files(parsed_args, include, exclude, silent)
 
+    # check that we can connect to PyPI
+    can_connect_to_pypi = _check_pypy_connection(silent)
+
     # check all requirements files
     requirements_to_fix = {}
     for req_file in files:
         needs_cleanup, requirements = _inspect_req_file(
-            req_file, parsed_args.only_sort, upstream_match, silent
+            req_file, parsed_args.only_sort, can_connect_to_pypi, upstream_match, silent
         )
         if needs_cleanup:
             requirements_to_fix[req_file] = requirements
@@ -81,26 +85,46 @@ def run(
     return 0 if success else 1
 
 
+def _check_pypy_connection(silent: bool) -> bool:
+    try:
+        urllib.request.urlopen("https://pypi.org/", timeout=1)
+        return True
+    except urllib.error.URLError:
+        if not silent:
+            warning = "Cannot connect to PyPI to identify package versions to pin."
+            print(check_utils.warning(warning))
+        return False
+
+
 def _inspect_req_file(
-    req_file: str, only_sort: bool, upstream_match: str, silent: bool
+    req_file: str,
+    only_sort: bool,
+    can_connect_to_pypi: bool,
+    upstream_match: str,
+    silent: bool,
 ) -> Tuple[bool, List[str]]:
     # read in requirements line-by-line
     with open(os.path.join(check_utils.root_dir, req_file), "r") as file:
         requirements = file.read().strip().split("\n")
 
     if not _are_pip_requirements(requirements):
-        error = f"{req_file} appears to contain lines that are not valid pip requirements"
+        error = (
+            f"{req_file} appears to contain lines that are not valid pip requirements"
+        )
         if req_file == "requirements.txt":
             raise SyntaxError(check_utils.failure(error))
         elif not silent:
             print(check_utils.warning(error))
-        return False, []  # file cannot be cleaned up, and there are no requirements to track
+        return (
+            False,
+            [],
+        )  # file cannot be cleaned up, and there are no requirements to track
 
     needs_cleanup, requirements = _sort_requirements(requirements)
     if needs_cleanup and not silent:
         print(check_utils.failure(f"{req_file} is not sorted."))
 
-    if not only_sort:
+    if not only_sort and can_connect_to_pypi:
         needs_cleanup |= _check_package_versions(
             req_file, requirements, upstream_match, silent, strict=True
         )
@@ -124,7 +148,9 @@ def _are_pip_requirements(requirements: List[str]) -> bool:
         (\.\*)?        # maybe once: .* (literal)
     """
     restriction = rf"{relation}\s*{version}"  # relation, maybe whitespace, and version
-    restrictions = rf"{restriction}(\s*,\s*{restriction})*"  # comma(+whitespace)-delineated list
+    restrictions = (
+        rf"{restriction}(\s*,\s*{restriction})*"  # comma(+whitespace)-delineated list
+    )
 
     comment = r"\# .*"  # literal "#" followed by anything
     pip_req_format = re.compile(
@@ -139,7 +165,9 @@ def _get_package_name(requirement: str) -> str:
 
 
 def _sort_requirements(requirements: List[str]) -> Tuple[bool, List[str]]:
-    sorted_requirements = sorted(requirements, key=lambda req: _get_package_name(req).lower())
+    sorted_requirements = sorted(
+        requirements, key=lambda req: _get_package_name(req).lower()
+    )
     needs_cleanup = requirements != sorted_requirements
     return needs_cleanup, sorted_requirements
 
@@ -161,7 +189,7 @@ def _check_package_versions(
             # this is not an upstream package
             continue
 
-        latest_version = _get_latest_version(package, silent)
+        latest_version = _get_latest_version(package)
         desired_req = f"{package}~={latest_version}"
         latest_version_is_required = desired_req == req
         up_to_date &= bool(latest_version_is_required)
@@ -172,59 +200,54 @@ def _check_package_versions(
         if not silent:
             if not latest_version_is_required:
                 should_require = desired_req.lstrip(package)
-                pin_text = f"{req_file} requires {req}, but should require {should_require}"
+                pin_text = (
+                    f"{req_file} requires {req}, but should require {should_require}"
+                )
                 print(text_format(pin_text))
 
             # check that a compatible version of this upstream package is installed locally
             _inspect_local_version(package, latest_version)
 
-    return strict and not up_to_date  # True iff we are *requiring* changes to the requirements file
+    return (
+        strict and not up_to_date
+    )  # True iff we are *requiring* changes to the requirements file
 
 
 @functools.lru_cache()
-def _get_latest_version(package: str, silent: bool) -> str:
-    """Retrieve the latest version of a package."""
-    local_version = _get_local_version(package)
-    pypi_version = _get_pypi_version(package, silent)
-    if not local_version and not pypi_version:
-        raise ModuleNotFoundError(f"Package not installed or found on PyPI: {package}")
-    return max(pypi_version or "0.0.0", local_version or "0.0.0", key=packaging.version.parse)
-
-
-def _get_local_version(package: str) -> Optional[str]:
-    """Retrieve the local version of a package (if installed)."""
-    base_package = package.split("[")[0]  # remove options: package_name[options] --> package_name
-    try:
-        return importlib.metadata.version(base_package)
-    except importlib.metadata.PackageNotFoundError:
-        return None
-
-
-def _get_pypi_version(package: str, silent: bool) -> Optional[str]:
-    """Retrieve the latest version of a package on PyPI (if found)."""
-    base_package = package.split("[")[0]  # remove options: package_name[options] --> package_name
+def _get_latest_version(package: str) -> str:
+    base_package = package.split("[")[
+        0
+    ]  # remove options: package_name[options] --> package_name
     pypi_url = f"https://pypi.org/pypi/{base_package}/json"
-    try:
-        package_info = urllib.request.urlopen(pypi_url).read().decode()
-        pypi_version = json.loads(package_info)["info"]["version"]
-        return pypi_version
-    except urllib.error.URLError:
-        if not silent:
-            warning = f"Cannot find package on PyPI: {base_package}."
-            print(check_utils.warning(warning))
-        return None
+    pypi_version = json.loads(urllib.request.urlopen(pypi_url).read().decode())["info"][
+        "version"
+    ]
+
+    # If the local *-superstaq version is newer, return that instead
+    return max(pypi_version, checks_superstaq.__version__, key=packaging.version.parse)
 
 
 def _inspect_local_version(package: str, latest_version: str) -> None:
-    """Check that the package is installed with the same minor version (X.Y.*) as latest_version."""
-    local_version = _get_local_version(package)
-    if not local_version or local_version.split(".")[:2] != latest_version.split(".")[:2]:
-        warning = f"WARNING: locally installed version of {package} is not up to date."
-        version_info = f"Local version is {local_version}, but latest version is {latest_version}."
+    base_package = package.split("[")[
+        0
+    ]  # remove options: package_name[options] --> package_name
+    try:
+        installed_info = subprocess.check_output(
+            [sys.executable, "-m", "pip", "show", base_package],
+            cwd=check_utils.root_dir,
+            text=True,
+        ).split()
+        installed_version = installed_info[installed_info.index("Version:") + 1]
+        # check that installed_version and latest_version have the same minor version: X.Y.*
+        assert installed_version.split(".")[:2] == latest_version.split(".")[:2]
+    except AssertionError:
+        warning = f"WARNING: locally installed {package} version is not up to date."
         suggestion = f"Try calling 'python -m pip install --upgrade {package}'."
         print(check_utils.warning(warning))
-        print(check_utils.warning(version_info))
         print(check_utils.warning(suggestion))
+    except subprocess.CalledProcessError:
+        # pip returned a non-zero exit status; let pip print its own error message
+        pass
 
 
 def _cleanup(
