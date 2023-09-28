529943b560a96273a9e1659ce8a6798c10d0184d
Kate S <kate.n.smith@outlook.com>
2023-09-28 18:24:13 -0500

    add dd_strategy

--- qiskit-superstaq/qiskit_superstaq/superstaq_backend.py
+++ qiskit-superstaq/qiskit_superstaq/superstaq_backend.py
@@ -262,6 +262,7 @@ class SuperstaqBackend(qiskit.providers.BackendV1):
         self,
         circuits: Union[qiskit.QuantumCircuit, Sequence[qiskit.QuantumCircuit]],
         dynamical_decoupling: bool = True,
+        dd_strategy: str = 'dynamic',
         **kwargs: Any,
     ) -> qss.compiler_output.CompilerOutput:
         """Compiles and optimizes the given circuit(s) for IBMQ devices.
@@ -269,6 +270,7 @@ class SuperstaqBackend(qiskit.providers.BackendV1):
         Args:
             circuits: The qiskit QuantumCircuit(s) to compile.
             dynamical_decoupling: Applies dynamical decoupling optimization to circuit(s).
+            dd_strategy: Method used for placing dynamical decoupling operations.
             kwargs: Other desired compile options.
 
         Returns:
