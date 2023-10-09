f0c48defdea6b591fc263a1daf8c06c42e2d5b4d
Paige <fredepai@umich.edu>
2023-10-09 17:53:02 -0500

    add qiskit to requirements

--- cirq-superstaq/cirq_superstaq/job_test.py
+++ cirq-superstaq/cirq_superstaq/job_test.py
@@ -145,11 +145,11 @@ def test_pulse_gate_circuit(job: css.job.Job) -> None:
 
     # The first call will trigger a refresh:
     with mocked_get_job_requests(job_dict) as mocked_get_job:
-        assert job.pulse_gate_circuits(index=0) == pulse_gate_circuit
+        assert job.pulse_gate_circuits()[0] == pulse_gate_circuit
         mocked_get_job.assert_called_once()
 
     # Shouldn't need to retrieve anything now that `job._job` is populated:
-    assert job.pulse_gate_circuits(index=0) == pulse_gate_circuit
+    assert job.pulse_gate_circuits()[0] == pulse_gate_circuit
 
 
 def test_multi_circuit_job() -> None:
