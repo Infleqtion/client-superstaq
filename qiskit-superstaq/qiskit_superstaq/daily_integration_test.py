0a2c98f166f969c00799c2fd19d02150d43e6e92
Victory Omole <vtomole2@gmail.com>
2023-10-16 17:06:32 -0500

    Don't check for `$` in daily integration test (#819)

--- qiskit-superstaq/qiskit_superstaq/daily_integration_test.py
+++ qiskit-superstaq/qiskit_superstaq/daily_integration_test.py
@@ -101,7 +101,7 @@ def test_aqt_compile_eca_regression(provider: qss.SuperstaqProvider) -> None:
 def test_get_balance(provider: qss.SuperstaqProvider) -> None:
     balance_str = provider.get_balance()
     assert isinstance(balance_str, str)
-    assert balance_str.endswith("credits")
+    assert "credits" in balance_str
 
     assert isinstance(provider.get_balance(pretty_output=False), float)
 
