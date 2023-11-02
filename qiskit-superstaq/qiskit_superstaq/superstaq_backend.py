4abb8ff44dfa0717119e96d7dbfbff652ad517ec
Colin_Campbell_CQ <colin.campbell@coldquanta.com>
2023-11-02 13:27:48 -0700

    included formatting

--- qiskit-superstaq/qiskit_superstaq/superstaq_backend.py
+++ qiskit-superstaq/qiskit_superstaq/superstaq_backend.py
@@ -53,7 +53,7 @@ class SuperstaqBackend(qiskit.providers.BackendV1):
             "memory": False,
             "max_shots": None,
             "coupling_map": None,
-            "description": f"{target_info.get('num_qubits')} qubit device"
+            "description": f"{target_info.get('num_qubits')} qubit device",
         }
         target_info.pop("target", None)
         target_info.pop("num_qubits", None)
