f163b8b05f93bceeec11845892e7124de8fa325a
Paige <fredepai@umich.edu>
2023-12-08 17:35:09 -0600

    remove check for refresh

--- cirq-superstaq/cirq_superstaq/job_test.py
+++ cirq-superstaq/cirq_superstaq/job_test.py
@@ -196,7 +196,6 @@ def test_pulse_gate_circuits_invalid_circuit(job: css.job.Job) -> None:
     with mocked_get_job_requests(job_dict) as mocked_get_job:
         with pytest.raises(ValueError, match="circuits could not be deserialized."):
             job.pulse_gate_circuits()
-            mocked_get_job.assert_called_once()
 
 
 def test_multi_pulse_gate_circuits(job: css.Job) -> None:
