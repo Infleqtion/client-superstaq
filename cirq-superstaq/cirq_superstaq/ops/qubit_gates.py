bfe8a4b486e19ed2d1fce11bb3b0531026967a1b
rich <rich@super.tech>
2024-02-13 23:42:43 -0600

    fix barrier symbol in transposed diagrams

--- cirq-superstaq/cirq_superstaq/ops/qubit_gates.py
+++ cirq-superstaq/cirq_superstaq/ops/qubit_gates.py
@@ -406,8 +406,11 @@ class Barrier(cirq.ops.IdentityGate, cirq.InterchangeableQubitsGate):
 
     def _circuit_diagram_info_(self, args: cirq.CircuitDiagramInfoArgs) -> tuple[str, ...]:
         if args.use_unicode_characters:
-            return ("│",) * self.num_qubits()
-        return ("|",) * self.num_qubits()
+            wire_symbol = "─" if args.transpose else "│"
+        else:
+            wire_symbol = "-" if args.transpose else "|"
+
+        return (wire_symbol,) * self.num_qubits()
 
 
 def barrier(*qubits: cirq.Qid) -> cirq.Operation:
