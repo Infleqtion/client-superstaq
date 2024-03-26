f41a20d016118f422e0ae60e30b9bfd9e26ea718
Michael A. Perlin <mika.perlin@gmail.com>
2024-03-26 17:21:22 -0500

    Update dev_tools/install.sh
    Co-authored-by: richrines1 <85512171+richrines1@users.noreply.github.com>

--- dev_tools/install.sh
+++ dev_tools/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-echo cd $(dirname $0)/../
+cd $(dirname $0)/..
 pip install -e ./checks-superstaq -e ./general-superstaq[dev] -e ./qiskit-superstaq[dev] -e ./cirq-superstaq[dev] -e ./supermarq-benchmarks[dev]
