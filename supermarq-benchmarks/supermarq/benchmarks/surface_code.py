69830712c54786e0fe93df1b52739e97189ccc4a
Michael A. Perlin <mika.perlin@gmail.com>
2024-03-26 15:43:52 -0500

    debugging code

--- supermarq-benchmarks/supermarq/benchmarks/surface_code.py
+++ supermarq-benchmarks/supermarq/benchmarks/surface_code.py
@@ -3,8 +3,7 @@ from typing import Literal
 
 import cirq
 import numpy as np
-
-# import qldpc
+import qldpc
 
 from supermarq.benchmark import Benchmark
 
@@ -59,7 +58,7 @@ class SurfaceCode(Benchmark):
         self.rows = num_rows
         self.cols = num_cols
         self.xzzx = xzzx
-        # self.code = qldpc.codes.SurfaceCode(num_rows, num_cols, rotated=True)
+        self.code = qldpc.codes.SurfaceCode(num_rows, num_cols, rotated=True)
 
     def circuit(self) -> cirq.Circuit:
         """Prepare a logical state and run one round of syndrome measurements."""
@@ -88,9 +87,9 @@ class SurfaceCode(Benchmark):
         for pauli, checks in [(cirq.X, self.code.matrix_x), (cirq.Z, self.code.matrix_z)]:
             checks = checks.reshape((-1, self.rows, self.cols))
             for ancilla, check in zip(self.get_ancilla_qubits(pauli), checks):
-                targets = [cirq.GridQubit(row, col) for row, col in zip(np.where(check))]
+                targets = [cirq.GridQubit(row, col) for row, col in zip(*np.where(check))]
                 pauli_ops = {qubit: pauli for qubit in targets}
-                pauli_string: cirq.tring[cirq.GridQubit] = cirq.tring(pauli_ops)
+                pauli_string: cirq.PauliString[cirq.GridQubit] = cirq.PauliString(pauli_ops)
                 circuit += pauli_string.controlled_by(ancilla)
 
         circuit += hadamards
@@ -100,7 +99,7 @@ class SurfaceCode(Benchmark):
         self, pauli: Literal[cirq.X, cirq.Z] | None = None
     ) -> list[cirq.NamedQubit]:
         """Get ancillas on this surface code."""
-        if pauli is not None:
+        if pauli is None:
             return self.get_ancilla_qubits(cirq.X) + self.get_ancilla_qubits(cirq.Z)
         num_checks = self.code.num_checks_x if pauli == cirq.X else self.code.num_checks_z
         return cirq.NamedQubit.range(num_checks, prefix=str(pauli))
@@ -136,6 +135,12 @@ class SurfaceCode(Benchmark):
                 circuit += cirq.H(qubit)
                 circuit += cirq.CX(qubit, qubit - (0, 1))
 
+        qubit_map = {
+            qubit: cirq.GridQubit(qubit.col, qubit.row)
+            for qubit in cirq.GridQubit.rect(self.rows, self.cols)
+        }
+        circuit = circuit.transform_qubits(qubit_map)
+
         if self.xzzx:
             # hadamard transform every other data qubit
             circuit += [
@@ -148,3 +153,22 @@ class SurfaceCode(Benchmark):
     def score(self, counts: dict[str, float]) -> float:
         """Benchmark score."""
         return NotImplemented
+
+
+code = SurfaceCode(3, 3, xzzx=False)
+
+circuit = code.prepare_logical_state()
+state = circuit.final_state_vector()
+
+for pauli, checks in [(cirq.X, code.code.matrix_x), (cirq.Z, code.code.matrix_z)]:
+    checks = checks.reshape((-1, code.rows, code.cols))
+    for idx, check in enumerate(checks):
+        targets = [cirq.GridQubit(row, col) for row, col in zip(*np.where(check))]
+        pauli_ops = {qubit: pauli for qubit in targets}
+        check_circuit = circuit + cirq.PauliString(pauli_ops)
+
+        check_state = check_circuit.final_state_vector()
+
+        print(pauli, idx)
+        print(check)
+        assert np.allclose(state, check_state)
