b96b40e6762856e3e2baf5086df938fffe4f1711
Michael A. Perlin <mika.perlin@gmail.com>
2024-03-26 16:02:17 -0500

    move install script to dev_tools

--- dev_tools/install.sh
+++ dev_tools/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-cd $(dirname $0)
+echo cd $(dirname $0)/../
 pip install -e ./checks-superstaq -e ./general-superstaq[dev] -e ./qiskit-superstaq[dev] -e ./cirq-superstaq[dev] -e ./superstaq-benchmarks[dev]
