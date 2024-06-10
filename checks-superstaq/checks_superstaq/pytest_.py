3e9c26234fddc8584d93cbb047fbcc47d6d2415a
Spencer Dearman <dearmanspencer@gmail.com>
2024-06-10 10:54:23 -0500

    Making an update that was not caught previously, logical updatE

--- checks-superstaq/checks_superstaq/pytest_.py
+++ checks-superstaq/checks_superstaq/pytest_.py
@@ -61,6 +61,7 @@ def run(
     if parsed_args.notebook:
         include = include or "*.ipynb"
     elif parsed_args.integration:
+        args_to_pass += ["--force-enable-socket"]
         include = include or "*_integration_test.py"
     else:
         include = include or "*.py"
@@ -71,7 +72,6 @@ def run(
     if parsed_args.notebook:
         args_to_pass += ["--nbmake", "--force-enable-socket"]
     elif not parsed_args.integration:
-        args_to_pass += ["--force-enable-socket"]
         files = check_utils.get_test_files(files, exclude=exclude, silent=silent)
 
     if not files:
