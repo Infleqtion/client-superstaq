c222cf5c763930528115a34c1fcb82989a5aa25f
Spencer Dearman <dearmanspencer@gmail.com>
2024-06-21 11:19:24 -0500

    Removing the outdated changes

--- checks/pytest_.py
+++ checks/pytest_.py
@@ -6,13 +6,6 @@ import checks_superstaq as checks
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-
-    # Checking if any argument matches the pattern *_integration_test.py
-    if "--integration" not in args and any(
-        re.match(r".*_integration_test\.py$", arg) for arg in args
-    ):
-        args.append("--integration")
-
     args += ["--exclude", "docs/source/apps/*"]
     args += ["--exclude", "docs/source/optimizations/ibm/ibmq_dd.ipynb"]
     exit(checks.pytest_.run(*args))
