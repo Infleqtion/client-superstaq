9630ccab09fa41ebda206c64c81acbe603bd2a07
nati <nwtbekele@gmail.com>
2024-07-01 16:29:02 -0500

    Plugin for checking explicit return type annotation for __init__

---
+++ checks-superstaq/checks_superstaq/init_return.py
@@ -0,0 +1,53 @@
+"""Check type type annotation of __init__ functions"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from astroid import Const, nodes
+from pylint.checkers import BaseChecker
+
+if TYPE_CHECKING:
+    from pylint.lint import PyLinter
+
+
+class InitChecker(BaseChecker):
+    """Checker for return type annotations for class constructors (__init__).
+
+    * Check that all __init__ functions have the return type annotation '-> None'.
+    """
+
+    name = "init-return-check"
+    msgs = {
+        "W9022": (
+            "Missing -> None return type annotation for __init__ method",
+            "missing-return-type-annotation-init",
+            "Missing type annotation for __init__ causes mypy check inconsistentcies.",
+        ),
+        "W9023": (
+            "Incorrect return type annotation for __init__ method. Expected None but found '%s'",
+            "incorrect-return-type-annotation-init",
+            "__init__ functions should only have return type None.",
+        ),
+    }
+
+    def visit_functiondef(self, node: nodes.FunctionDef) -> None:
+        """Called for function and method definitions (def).
+        Checks if the return type annotation for __init__ is explicity None or incorrect.
+
+        Args:
+            node: Node for a function or method definition in the AST
+        """
+        if node.name == "__init__":
+            if not node.returns:
+                self.add_message("missing-return-type-annotation-init", node=node)
+            elif not isinstance(node.returns, Const):
+                self.add_message(
+                    "incorrect-return-type-annotation-init",
+                    args=node.returns.as_string(),
+                    node=node,
+                )
+
+
+def register(linter: PyLinter) -> None:
+    linter.register_checker(InitChecker(linter))
