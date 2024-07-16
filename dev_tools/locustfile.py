50750a54ee42bd828fde602beb5fd660ce6906bc
nati <nwtbekele@gmail.com>
2024-07-16 17:24:16 -0500

    Fix license headers for repo

--- dev_tools/locustfile.py
+++ dev_tools/locustfile.py
@@ -1,3 +1,16 @@
+# Copyright 2024 Infleqtion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Locust (https://docs.locust.io/en/stable/what-is-locust.html) is a load testing tool.
 To run Locust, first install it with "python -m pip install locust", set the SUPERSTAQ_API_KEY
 environment variable and execute "locust --config=locust.conf" in the "dev_tools" directory.
