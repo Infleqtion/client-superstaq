3bd50cf8491647ba182aa3c20b69c7adeef9b1e2
Cameron Booker <cameron.booker@infleqtion.com>
2024-08-07 17:14:50 +0100

    Initial implementation

--- supermarq-benchmarks/supermarq/qcvv/__init__.py
+++ supermarq-benchmarks/supermarq/qcvv/__init__.py
@@ -1,5 +1,6 @@
 """A toolkit of QCVV routines."""
 
 from .base_experiment import BenchmarkingExperiment, BenchmarkingResults, Sample
+from .su2 import SU2, SU2Results
 
-__all__ = ["BenchmarkingExperiment", "BenchmarkingResults", "Sample"]
+__all__ = ["BenchmarkingExperiment", "BenchmarkingResults", "Sample", "SU2", "SU2Results"]
