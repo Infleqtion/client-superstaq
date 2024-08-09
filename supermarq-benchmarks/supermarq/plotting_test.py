d51c76d305a6378160f837c2e8b22a48c868f2e2
Bharath <bharath.thotakura@infleqtion.com>
2024-08-09 11:43:16 -0500

    update tests

--- supermarq-benchmarks/supermarq/plotting_test.py
+++ supermarq-benchmarks/supermarq/plotting_test.py
@@ -1,38 +1,25 @@
 # pylint: disable=missing-function-docstring,missing-class-docstring
 from __future__ import annotations
 
-import pytest
-
 import supermarq
 
 
 def test_plot_benchmark() -> None:
     supermarq.plotting.plot_benchmark(
-        ["test title", ["b1", "b2", "b3"], [[0.1, 0.1, 0.1], [0.2, 0.2, 0.2], [0.3, 0.3, 0.3]]],
+        "test title",
+        ["b1", "b2", "b3"],
+        [[0.1, 0.1, 0.1], [0.2, 0.2, 0.2], [0.3, 0.3, 0.3]],
         spoke_labels=["f1", "f2", "f3"],
         show=False,
     )
 
     supermarq.plotting.plot_benchmark(
-        [
-            "test title",
-            ["b1", "b2", "b3", "b4", "b5"],
-            [[0.1] * 5, [0.2] * 5, [0.3] * 5, [0.4] * 5, [0.5] * 5],
-        ],
+        "test title",
+        ["b1", "b2", "b3", "b4", "b5"],
+        [[0.1] * 5, [0.2] * 5, [0.3] * 5, [0.4] * 5, [0.5] * 5],
         spoke_labels=None,
         show=False,
     )
-
-    with pytest.raises(ValueError, match="Invalid plot title"):
-        supermarq.plotting.plot_benchmark(
-            [
-                ["b1", "b2", "b3"],
-                ["b1", "b2", "b3"],
-                [[0.1, 0.1, 0.1], [0.2, 0.2, 0.2], [0.3, 0.3, 0.3]],
-            ],
-            spoke_labels=["f1", "f2", "f3"],
-            show=False,
-        )
 
 
 def test_plot_volumetric_results() -> None:
