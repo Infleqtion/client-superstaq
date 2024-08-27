6c23e4db53699e7f7e272b81554f6091708479cf
Cameron Booker <cameron.booker@infleqtion.com>
2024-08-27 17:40:17 +0100

    Initial draft

--- supermarq-benchmarks/supermarq/qcvv/__init__.py
+++ supermarq-benchmarks/supermarq/qcvv/__init__.py
@@ -2,10 +2,13 @@
 
 from .base_experiment import BenchmarkingExperiment, BenchmarkingResults, Sample
 from .xeb import XEB, XEBResults, XEBSample
+from .cb import CB, CBResults
 
 __all__ = [
     "BenchmarkingExperiment",
     "BenchmarkingResults",
+    "CB",
+    "CBResults",
     "Sample",
     "XEB",
     "XEBResults",
