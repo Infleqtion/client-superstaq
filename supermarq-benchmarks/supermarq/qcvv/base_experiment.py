b237d8c6a6a7468413c6bc72695485da94902585
Bharath Thotakura <113555655+bharat-thotakura@users.noreply.github.com>
2025-01-30 13:37:05 -0600

    Format update from new `black` version  (#1151)
    Fixes format check failures in CI
    
    Signed-off-by: Bharath <bharath.thotakura@infleqtion.com>

--- supermarq-benchmarks/supermarq/qcvv/base_experiment.py
+++ supermarq-benchmarks/supermarq/qcvv/base_experiment.py
@@ -10,8 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Base experiment class and tools used across all experiments.
-"""
+"""Base experiment class and tools used across all experiments."""
 from __future__ import annotations
 
 import functools
