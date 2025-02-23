cbe13bef52e6b60f0135b1083db0a42822dd02af
David Owusu-Antwi <david@super.tech>
2025-02-23 21:28:41 +0000

    add jacobian and hessian calculations for optimizers that use them

--- Infleqtion-DCM4-Challenge/tools.py
+++ Infleqtion-DCM4-Challenge/tools.py
@@ -15,6 +15,28 @@ import matplotlib.patches as mpatches
 from matplotlib.ticker import MaxNLocator
 import scipy
 
+def JACOBIAN(x):
+    """Rosenbrock Jacobian. See https://docs.scipy.org/doc/scipy/tutorial/optimize.html"""
+    xm = x[1:-1]
+    xm_m1 = x[:-2]
+    xm_p1 = x[2:]
+    der = np.zeros_like(x)
+    der[1:-1] = 200*(xm-xm_m1**2) - 400*(xm_p1 - xm**2)*xm - 2*(1-xm)
+    der[0] = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
+    der[-1] = 200*(x[-1]-x[-2]**2)
+    return der
+
+
+def HESSIAN(x):
+    """Rosenbrock Hessian. See https://docs.scipy.org/doc/scipy/tutorial/optimize.html."""
+    x = np.asarray(x)
+    H = np.diag(-400*x[:-1],1) - np.diag(400*x[:-1],-1)
+    diagonal = np.zeros_like(x)
+    diagonal[0] = 1200*x[0]**2-400*x[1]+2
+    diagonal[-1] = 200
+    diagonal[1:-1] = 202 + 1200*x[1:-1]**2 - 400*x[2:]
+    H = H + np.diag(diagonal)
+    return H
 
 class AirDefenceModel:
     def __init__(
@@ -298,8 +320,22 @@ class AirDefenceModel:
         plt.show()
 
     def solve_qaoa(
-        self, nreps=8, x0=None, method="COBYQA", shots=0, tol=1e-5, maxiter=None, **kwargs
+        self,
+        nreps=8,
+        x0=None,
+        method="COBYQA",
+        shots=0,
+        tol=1e-5,
+        maxiter=None,
+        jacobian=None,
+        hessian=None,
+        **kwargs,
     ):
+
+        if method in [
+            'Newton-CG', 'trust-constr', 'dogleg', 'trust-ncg', 'trust-exact', 'trust-krylov'
+        ]:
+            jacobian, hessian = JACOBIAN, HESSIAN
 
         self.build_circuit(num_reps=nreps, store=True)
 
@@ -316,6 +352,8 @@ class AirDefenceModel:
             tol=tol,
             options={"maxiter": maxiter},
             callback=self.callback,
+            jac=jacobian,
+            hess=hessian,
             **kwargs,
         )
         return res
