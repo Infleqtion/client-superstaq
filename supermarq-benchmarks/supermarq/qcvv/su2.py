ae49c4bd28edb61dfe637ea70ca4c9ed31e41c9d
Cameron Booker <cameron.booker@infleqtion.com>
2025-03-10 14:11:12 +0000

    Nit

--- supermarq-benchmarks/supermarq/qcvv/su2.py
+++ supermarq-benchmarks/supermarq/qcvv/su2.py
@@ -10,8 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tooling for SU(2) benchmarking
-"""
+"""Tooling for SU(2) benchmarking"""
 
 from __future__ import annotations
 
