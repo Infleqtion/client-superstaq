4e2e2729b8f420437821abca4eda1aab4bd94096
Viet Pham Ngoc <viphamngoc@gmail.com>
2025-06-12 09:01:13 +0100

    coverage test for provider

--- qiskit-superstaq/qiskit_superstaq/superstaq_provider_test.py
+++ qiskit-superstaq/qiskit_superstaq/superstaq_provider_test.py
@@ -43,6 +43,13 @@ def test_provider_args() -> None:
         "ibmq_instance": "instance",
         "ibmq_token": "token",
     }
+    assert isinstance(ss_provider._client, gss.superstaq_client._SuperstaqClient)
+
+    ss_provider = qss.SuperstaqProvider(api_version="v0.3.0")
+    assert isinstance(ss_provider._client, gss.superstaq_client._SuperstaqClientV3)
+
+    with pytest.raises(ValueError, match="`api_version` can only take value 'v0.2.0' or 'v0.3.0'"):
+        ss_provider = qss.SuperstaqProvider(api_version="v0.4.0")
 
 
 @patch.dict(os.environ, {"SUPERSTAQ_API_KEY": ""})
