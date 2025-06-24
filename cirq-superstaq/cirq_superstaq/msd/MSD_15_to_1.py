1ddfb68873448d04ca934ee8b28162d27fde6c78
abyssaldragonz <jbl.noetic@gmail.com>
2025-06-24 13:18:33 -0500

    reformatting msd circuits

--- cirq-superstaq/cirq_superstaq/msd/MSD_15_to_1.py
+++ cirq-superstaq/cirq_superstaq/msd/MSD_15_to_1.py
@@ -1,72 +1,115 @@
+# ruff: noqa: ERA001, T201
+
 import cirq
-from cirq.circuits import InsertStrategy
 import sympy
+from cirq.circuits import InsertStrategy
 
 # index 15 is our magic state
 qubits = cirq.LineQubit.range(16)
 
 cir = cirq.Circuit()
 
-# reset qubits
-for q in qubits:
-    cir.append([cirq.R(q)])
 
-# set ket plus using hadamard
-cir.append([cirq.H(qubits[0]),
+# Function to perform a 15-to-1 magic state distillation protocol
+# @param: qubits - List of LineQubits with length 16.
+#         The last qubit should be the final magic state qubit.
+# @return: Circuit - the magic state distillation circuit
+def msd_15_to_1(qubits: list[cirq.LineQubit]) -> cirq.Circuit:
+    """Function to perform a 15-to-1 magic state distillation protocol.
+
+    Args:
+        qubits: The list of LineQubits of length 16.
+                The last qubit will be the final magic state qubit.
+
+    Returns:
+        The magic state distillation circuit.
+    """
+    # reset qubits
+    for q in qubits:
+        cir.append([cirq.R(q)])
+
+    # set ket plus using hadamard
+    cir.append(
+        [
+            cirq.H(qubits[0]),
             cirq.H(qubits[1]),
             cirq.H(qubits[3]),
             cirq.H(qubits[7]),
-            cirq.H(qubits[15])], 
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.H(qubits[15]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-# control qubits are @
-# target qubits are X 
-cir.append([cirq.CNOT(qubits[15], qubits[14]),
+    # control qubits are @
+    # target qubits are X
+    cir.append(
+        [
+            cirq.CNOT(qubits[15], qubits[14]),
             cirq.CNOT(qubits[7], qubits[8]),
             cirq.CNOT(qubits[7], qubits[9]),
             cirq.CNOT(qubits[7], qubits[10]),
             cirq.CNOT(qubits[7], qubits[11]),
             cirq.CNOT(qubits[7], qubits[12]),
             cirq.CNOT(qubits[7], qubits[13]),
-            cirq.CNOT(qubits[7], qubits[14])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.CNOT(qubits[7], qubits[14]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.CNOT(qubits[3], qubits[4]),
+    cir.append(
+        [
+            cirq.CNOT(qubits[3], qubits[4]),
             cirq.CNOT(qubits[3], qubits[5]),
             cirq.CNOT(qubits[3], qubits[6]),
             cirq.CNOT(qubits[3], qubits[11]),
             cirq.CNOT(qubits[3], qubits[12]),
             cirq.CNOT(qubits[3], qubits[13]),
-            cirq.CNOT(qubits[3], qubits[14])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.CNOT(qubits[3], qubits[14]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.CNOT(qubits[1], qubits[2]),
+    cir.append(
+        [
+            cirq.CNOT(qubits[1], qubits[2]),
             cirq.CNOT(qubits[1], qubits[5]),
             cirq.CNOT(qubits[1], qubits[6]),
             cirq.CNOT(qubits[1], qubits[9]),
             cirq.CNOT(qubits[1], qubits[10]),
             cirq.CNOT(qubits[1], qubits[13]),
-            cirq.CNOT(qubits[1], qubits[14])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.CNOT(qubits[1], qubits[14]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.CNOT(qubits[0], qubits[2]),
+    cir.append(
+        [
+            cirq.CNOT(qubits[0], qubits[2]),
             cirq.CNOT(qubits[0], qubits[4]),
             cirq.CNOT(qubits[0], qubits[6]),
             cirq.CNOT(qubits[0], qubits[8]),
             cirq.CNOT(qubits[0], qubits[10]),
             cirq.CNOT(qubits[0], qubits[12]),
-            cirq.CNOT(qubits[0], qubits[14])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.CNOT(qubits[0], qubits[14]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.CNOT(qubits[14], qubits[2]),
+    cir.append(
+        [
+            cirq.CNOT(qubits[14], qubits[2]),
             cirq.CNOT(qubits[14], qubits[4]),
             cirq.CNOT(qubits[14], qubits[5]),
             cirq.CNOT(qubits[14], qubits[8]),
             cirq.CNOT(qubits[14], qubits[9]),
-            cirq.CNOT(qubits[14], qubits[11])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.CNOT(qubits[14], qubits[11]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.inverse(cirq.T(qubits[0])),
+    cir.append(
+        [
+            cirq.inverse(cirq.T(qubits[0])),
             cirq.inverse(cirq.T(qubits[1])),
             cirq.inverse(cirq.T(qubits[2])),
             cirq.inverse(cirq.T(qubits[3])),
@@ -80,10 +123,14 @@ cir.append([cirq.inverse(cirq.T(qubits[0])),
             cirq.inverse(cirq.T(qubits[11])),
             cirq.inverse(cirq.T(qubits[12])),
             cirq.inverse(cirq.T(qubits[13])),
-            cirq.inverse(cirq.T(qubits[14])),],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.inverse(cirq.T(qubits[14])),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.H(qubits[0]),
+    cir.append(
+        [
+            cirq.H(qubits[0]),
             cirq.H(qubits[1]),
             cirq.H(qubits[2]),
             cirq.H(qubits[3]),
@@ -98,61 +145,71 @@ cir.append([cirq.H(qubits[0]),
             cirq.H(qubits[12]),
             cirq.H(qubits[13]),
             cirq.H(qubits[14]),
-            cirq.measure(qubits[0],  key='m1'),
-            cirq.measure(qubits[1],  key='m2'),
-            cirq.measure(qubits[2],  key='m3'),
-            cirq.measure(qubits[3],  key='m4'),
-            cirq.measure(qubits[4],  key='m5'),
-            cirq.measure(qubits[5],  key='m6'),
-            cirq.measure(qubits[6],  key='m7'),
-            cirq.measure(qubits[7],  key='m8'),
-            cirq.measure(qubits[8],  key='m9'),
-            cirq.measure(qubits[9],  key='m10'),
-            cirq.measure(qubits[10], key='m11'),
-            cirq.measure(qubits[11], key='m12'),
-            cirq.measure(qubits[12], key='m13'),
-            cirq.measure(qubits[13], key='m14'),
-            cirq.measure(qubits[14], key='m15'),],  # no need to measure index 15, that is our magic state
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.measure(qubits[0], key="m1"),
+            cirq.measure(qubits[1], key="m2"),
+            cirq.measure(qubits[2], key="m3"),
+            cirq.measure(qubits[3], key="m4"),
+            cirq.measure(qubits[4], key="m5"),
+            cirq.measure(qubits[5], key="m6"),
+            cirq.measure(qubits[6], key="m7"),
+            cirq.measure(qubits[7], key="m8"),
+            cirq.measure(qubits[8], key="m9"),
+            cirq.measure(qubits[9], key="m10"),
+            cirq.measure(qubits[10], key="m11"),
+            cirq.measure(qubits[11], key="m12"),
+            cirq.measure(qubits[12], key="m13"),
+            cirq.measure(qubits[13], key="m14"),
+            cirq.measure(qubits[14], key="m15"),
+        ],
+        # no need to measure index 15, that is our magic state
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-print("CIRCUIT ====================")
-print(cir)
+    # print("CIRCUIT ====================")
+    # print(cir)
 
+    # index 15=magic
+    m1, m2, m3, m4, m5, m6, m7, m8 = sympy.symbols("m1 m2 m3 m4 m5 m6 m7 m8")
+    m9, m10, m11, m12, m13, m14, m15 = sympy.symbols("m9 m10 m11 m12 m13 m14 m15")
+    # all those parities must be 0 for it to be even ( output of xor is 0 for even )
+    # so stop when true == ~0 & ~0 & ~0 & ~0 == ~(0 | 0 | 0 | 0)
+    # use demorgans
+    # https://arxiv.org/ftp/arxiv/papers/1208/1208.0928.pdf page 38 figure 33
+    evenParity = sympy.Not(
+        sympy.Xor(m4, m5, m6, m7, m8, m9, m10, m11)
+        | sympy.Xor(m1, m2, m3, m4, m5, m6, m7, m15)
+        | sympy.Xor(m2, m3, m4, m5, m10, m11, m12, m13)
+        | sympy.Xor(m1, m2, m5, m6, m9, m10, m13, m14)
+    )
+    # if special parity of all qubits is ODD, do Z
+    specialParity = sympy.Xor(m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15)
 
-# index 15=magic
-m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15 = sympy.symbols('m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11 m12 m13 m14 m15')
-# all those parities must be 0 for it to be even ( output of xor is 0 for even )
-# so stop when true == ~0 & ~0 & ~0 & ~0 == ~(0 | 0 | 0 | 0)
-# use demorgans
-# https://arxiv.org/ftp/arxiv/papers/1208/1208.0928.pdf page 38 figure 33
-evenParity = sympy.Not(sympy.Xor(m4,m5,m6,m7,m8,m9,m10,m11) | 
-                       sympy.Xor(m1,m2,m3,m4,m5,m6,m7,m15) | 
-                       sympy.Xor(m2,m3,m4,m5,m10,m11,m12,m13) | 
-                       sympy.Xor(m1,m2,m5,m6,m9,m10,m13,m14))
-# if special parity of all qubits is ODD, do Z 
-specialParity = sympy.Xor(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15)
-
-sim = cirq.Simulator()
-
-# repeating until matched parities are all even
-magicStateCir = cirq.Circuit(
+    # repeating until matched parities are all even
+    magicStateCir = cirq.Circuit(
         cirq.CircuitOperation(
-            circuit=cir.freeze(), 
+            circuit=cir.freeze(),
             use_repetition_ids=False,
-            repeat_until=cirq.SympyCondition(evenParity)
+            repeat_until=cirq.SympyCondition(evenParity),
         )
         # put corrections
     )
 
-# magic state "correction"
-# is odd, so do the Z
-magicStateCir.append([cirq.Z(qubits[15]).with_classical_controls(specialParity)])
+    # magic state "correction"
+    # is odd, so do the Z
+    magicStateCir.append([cirq.Z(qubits[15]).with_classical_controls(specialParity)])
 
+    # return circuit
+    return magicStateCir
+
+
+# TESTING
+sim = cirq.Simulator()
+magicStateCir = msd_15_to_1(qubits)
 print(magicStateCir)
 
 results = sim.simulate(magicStateCir)
 print("RESULTS ====================")
-print(results, end='\n\n')
+print(results, end="\n\n")
 print("FINAL STATE VECTOR ====================")
 stateVector = results.final_state_vector
 # print(stateVector)
@@ -162,23 +219,41 @@ print(cirq.dirac_notation(stateVector))
 print("\n\nSIMULATING ====================")
 
 
-fMap = {}
+fMap: dict[str, int] = {}
 even_special_count = 0
 odd_special_count = 0
 
 # looking for 0.71|0⟩ + (0.5+0.5j)|1⟩
-for i in range(1000):
+for _i in range(1000):
     simResults = sim.simulate(magicStateCir)
-    targetTensorResults = cirq.dirac_notation(simResults.get_state_containing_qubit(cirq.q(15)).target_tensor)
-    m_o_q = "" # measurements_of_qubits 
+    targetTensorResults = cirq.dirac_notation(
+        simResults.get_state_containing_qubit(cirq.q(15)).target_tensor
+    )
+    m_o_q: str = ""  # measurements_of_qubits
     # print(simResults.measurements)
     for m in simResults.measurements:
         m_o_q += str(simResults.measurements[m][0])
     # print(m_o_q + "\n")
 
-    if (sympy.Xor(m_o_q[0],m_o_q[1],m_o_q[2],m_o_q[3],m_o_q[4],m_o_q[5],m_o_q[6],m_o_q[7],m_o_q[8],m_o_q[9],m_o_q[10],m_o_q[11],m_o_q[12],m_o_q[13],m_o_q[14])): # is odd
+    if sympy.Xor(
+        m_o_q[0],
+        m_o_q[1],
+        m_o_q[2],
+        m_o_q[3],
+        m_o_q[4],
+        m_o_q[5],
+        m_o_q[6],
+        m_o_q[7],
+        m_o_q[8],
+        m_o_q[9],
+        m_o_q[10],
+        m_o_q[11],
+        m_o_q[12],
+        m_o_q[13],
+        m_o_q[14],
+    ):  # is odd
         odd_special_count += 1
-    else: # is even
+    else:  # is even
         even_special_count += 1
 
     if (targetTensorResults) in fMap:
@@ -188,9 +263,8 @@ for i in range(1000):
     # print(targetTensorResults)
 
 print("CHECKING PARITY OF ALL QUBITS ====================")
-print("EVEN SPECIAL PARITY:\t" + str(even_special_count)) # should give us 0.71|0⟩ + (0.5+0.5j)|1⟩
-print("ODD SPECIAL PARITY:\t" + str(odd_special_count)) # should give us 0.71|0⟩ + (-0.5-0.5j)|1⟩
-
+print("EVEN SPECIAL PARITY:\t" + str(even_special_count))  # should give us 0.71|0⟩ + (0.5+0.5j)|1⟩
+print("ODD SPECIAL PARITY:\t" + str(odd_special_count))  # should give us 0.71|0⟩ + (-0.5-0.5j)|1⟩
 
 print("\nFINAL STATE VECTORS FREQUENCIES ====================")
 for f in fMap:
@@ -202,9 +276,9 @@ densityMatrix = cirq.density_matrix_from_state_vector(stateVector, indices=[15])
 print(densityMatrix)
 
 print("\nSINGLE QUBIT TOMOGRAPHY ====================")
-tomography_result = cirq.experiments.single_qubit_state_tomography(sim, qubits[15], magicStateCir, 10000) 
-print(tomography_result.data, end="\n\n\n")
+tomo_res = cirq.experiments.single_qubit_state_tomography(sim, qubits[15], magicStateCir, 10000)
+print(tomo_res.data, end="\n\n\n")
 
 # from IPython.display import FileLink
-# cirq.to_json(magicStateCir, file_or_fn='MSD_15_to_1.json')
-# FileLink('MSD_15_to_1.json')
+# cirq.to_json(magicStateCir, file_or_fn="MSD_15_to_1.json")
+# FileLink("MSD_15_to_1.json")
--- cirq-superstaq/cirq_superstaq/msd/MSD_7_to_1.py
+++ cirq-superstaq/cirq_superstaq/msd/MSD_7_to_1.py
@@ -1,108 +1,158 @@
+# ruff: noqa: ERA001, T201
+
 import cirq
-from cirq.circuits import InsertStrategy
 import sympy
+from cirq.circuits import InsertStrategy
 
 # index 0 is our magic state
 qubits = cirq.LineQubit.range(8)
 
 cir = cirq.Circuit()
 
-# reset qubits
-for q in qubits:
-    cir.append([cirq.R(q)])
 
-# set ket plus using hadamard
-cir.append([cirq.H(qubits[0]),
+# Function to perform a 7-to-1 magic state distillation protocol
+# @param: qubits - List of LineQubits with length 8.
+#         The first qubit should be the final magic state qubit.
+# @return: Circuit - the magic state distillation circuit
+def msd_7_to_1(qubits: list[cirq.LineQubit]) -> cirq.Circuit:
+    """Function to perform a 7-to-1 magic state distillation protocol.
+
+    Args:
+        qubits: The list of LineQubits of length 8.
+                The first qubit will be the final magic state qubit.
+
+    Returns:
+        The magic state distillation circuit.
+    """
+    # reset qubits
+    for q in qubits:
+        cir.append([cirq.R(q)])
+
+    # set ket plus using hadamard
+    cir.append(
+        [
+            cirq.H(qubits[0]),
             cirq.H(qubits[1]),
             cirq.H(qubits[2]),
-            cirq.H(qubits[4])], 
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.H(qubits[4]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-# control qubits are @
-# target qubits are X 
-cir.append([cirq.CNOT(qubits[0], qubits[3])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+    # control qubits are @
+    # target qubits are X
+    cir.append([cirq.CNOT(qubits[0], qubits[3])], strategy=InsertStrategy.NEW_THEN_INLINE)
 
-cir.append([cirq.CNOT(qubits[3], qubits[5]),
-            cirq.CNOT(qubits[3], qubits[6])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+    cir.append(
+        [
+            cirq.CNOT(qubits[3], qubits[5]),
+            cirq.CNOT(qubits[3], qubits[6]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.CNOT(qubits[4], qubits[5]),
+    cir.append(
+        [
+            cirq.CNOT(qubits[4], qubits[5]),
             cirq.CNOT(qubits[4], qubits[6]),
-            cirq.CNOT(qubits[4], qubits[7])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.CNOT(qubits[4], qubits[7]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.CNOT(qubits[2], qubits[3]),
+    cir.append(
+        [
+            cirq.CNOT(qubits[2], qubits[3]),
             cirq.CNOT(qubits[2], qubits[6]),
-            cirq.CNOT(qubits[2], qubits[7])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.CNOT(qubits[2], qubits[7]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-cir.append([cirq.CNOT(qubits[1], qubits[3]),
+    cir.append(
+        [
+            cirq.CNOT(qubits[1], qubits[3]),
             cirq.CNOT(qubits[1], qubits[5]),
-            cirq.CNOT(qubits[1], qubits[7])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.CNOT(qubits[1], qubits[7]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-# adding S gate
-cir.append([cirq.S(qubits[1]),
+    # adding S gate
+    cir.append(
+        [
+            cirq.S(qubits[1]),
             cirq.S(qubits[2]),
             cirq.S(qubits[3]),
             cirq.S(qubits[4]),
             cirq.S(qubits[5]),
             cirq.S(qubits[6]),
-            cirq.S(qubits[7])],
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.S(qubits[7]),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-# no need to measure index 0, that is our magic state
-cir.append([cirq.H(qubits[1]),
+    # no need to measure index 0, that is our magic state
+    cir.append(
+        [
+            cirq.H(qubits[1]),
             cirq.H(qubits[2]),
             cirq.H(qubits[3]),
             cirq.H(qubits[4]),
             cirq.H(qubits[5]),
             cirq.H(qubits[6]),
             cirq.H(qubits[7]),
-            cirq.measure(qubits[1], key='m6'),
-            cirq.measure(qubits[2], key='m5'),
-            cirq.measure(qubits[3], key='m4'),
-            cirq.measure(qubits[4], key='m3'),
-            cirq.measure(qubits[5], key='m2'),
-            cirq.measure(qubits[6], key='m1'),
-            cirq.measure(qubits[7], key='m0'),], 
-        strategy=InsertStrategy.NEW_THEN_INLINE)
+            cirq.measure(qubits[1], key="m6"),
+            cirq.measure(qubits[2], key="m5"),
+            cirq.measure(qubits[3], key="m4"),
+            cirq.measure(qubits[4], key="m3"),
+            cirq.measure(qubits[5], key="m2"),
+            cirq.measure(qubits[6], key="m1"),
+            cirq.measure(qubits[7], key="m0"),
+        ],
+        strategy=InsertStrategy.NEW_THEN_INLINE,
+    )
 
-print("CIRCUIT ====================")
-print(cir)
+    # print("CIRCUIT ====================")
+    # print(cir)
 
+    # index 0=magic
+    m0, m1, m2, m3, m4, m5, m6 = sympy.symbols("m0 m1 m2 m3 m4 m5 m6")
+    # all those parities must be 0 for it to be even ( so output of xor is 0 for even )
+    # so stop when true == ~0 & ~0 & ~0 == ~(0 | 0 | 0)
+    # use demorgans
+    # https://arxiv.org/pdf/0803.0272 page 12 figure 19
+    evenParity = sympy.Not(
+        sympy.Xor(m0, m1, m2, m3) | sympy.Xor(m0, m1, m4, m5) | sympy.Xor(m0, m2, m4, m6)
+    )
+    # if special parity is EVEN, do Z
+    specialParity = sympy.Xor(m4, m5, m6)
 
-# index 0=magic
-m0, m1, m2, m3, m4, m5, m6 = sympy.symbols('m0 m1 m2 m3 m4 m5 m6')
-# all those parities must be 0 for it to be even ( so output of xor is 0 for even )
-# so stop when true == ~0 & ~0 & ~0 == ~(0 | 0 | 0)
-# use demorgans
-# https://arxiv.org/pdf/0803.0272 page 12 figure 19
-evenParity = sympy.Not(sympy.Xor(m0,m1,m2,m3) | sympy.Xor(m0,m1,m4,m5) | sympy.Xor(m0,m2,m4,m6))
-# if special parity is EVEN, do Z 
-specialParity = sympy.Xor(m4,m5,m6)
-
-sim = cirq.Simulator()
-
-# repeating until matched parities
-magicStateCir = cirq.Circuit(
+    # repeating until matched parities
+    magicStateCir = cirq.Circuit(
         cirq.CircuitOperation(
-            circuit=cir.freeze(), 
+            circuit=cir.freeze(),
             use_repetition_ids=False,
-            repeat_until=cirq.SympyCondition(evenParity)
+            repeat_until=cirq.SympyCondition(evenParity),
         )
     )
 
-# magic state "correction"
-# is even, so do the Z
-magicStateCir.append([cirq.Z(qubits[0]).with_classical_controls(sympy.Not(specialParity))])
+    # magic state "correction"
+    # is even, so do the Z
+    magicStateCir.append([cirq.Z(qubits[0]).with_classical_controls(sympy.Not(specialParity))])
 
+    # return circuit
+    return magicStateCir
+
+
+# TESTING
+sim = cirq.Simulator()
+magicStateCir = msd_7_to_1(qubits)
 print(magicStateCir)
 
 results = sim.simulate(magicStateCir)
 print("\nRESULTS ====================")
-print(results, end='\n\n')
+print(results, end="\n\n")
 print("\nFINAL STATE VECTOR =========")
 stateVector = results.final_state_vector
 # print(stateVector)
@@ -113,9 +163,11 @@ print("\n\nSIMULATING ====================")
 
 
 # looking for 0.71|0⟩ + (0.71j)|1⟩
-fMap = {}
-for i in range(1000):
-    targetTensorResults = cirq.dirac_notation(sim.simulate(magicStateCir).get_state_containing_qubit(cirq.q(0)).target_tensor)
+fMap: dict[str, int] = {}
+for _i in range(1000):
+    targetTensorResults = cirq.dirac_notation(
+        sim.simulate(magicStateCir).get_state_containing_qubit(cirq.q(0)).target_tensor
+    )
     if targetTensorResults in fMap:
         fMap[targetTensorResults] += 1
     else:
@@ -132,9 +184,9 @@ densityMatrix = cirq.density_matrix_from_state_vector(stateVector, indices=[0])
 print(densityMatrix)
 
 print("\nSINGLE QUBIT TOMOGRAPHY ====================")
-tomography_result = cirq.experiments.single_qubit_state_tomography(sim, qubits[0], magicStateCir, 10000) 
-print(tomography_result.data)
+tomo_res = cirq.experiments.single_qubit_state_tomography(sim, qubits[0], magicStateCir, 10000)
+print(tomo_res.data)
 
 # from IPython.display import FileLink
-# cirq.to_json(magicStateCir, file_or_fn='MSD_7_to_1.json')
-# FileLink('MSD_7_to_1.json')
+# cirq.to_json(magicStateCir, file_or_fn="MSD_7_to_1.json")
+# FileLink("MSD_7_to_1.json")
