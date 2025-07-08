367b5b55cb1cdeac68d8233c5f3c6f0892f22f89
vtomole <vtomole2@gmail.com>
2025-07-08 17:14:30 -0500

    Licences

--- cirq-superstaq/cirq_superstaq/__init__.py
+++ cirq-superstaq/cirq_superstaq/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cirq_superstaq import compiler_output, evaluation, validation
+from cirq_superstaq import compiler_output, evaluation, resource_counters, validation
 from cirq_superstaq._version import __version__
 from cirq_superstaq.compiler_output import active_qubit_indices, measured_qubit_indices
 from cirq_superstaq.job import Job
@@ -112,5 +112,6 @@ __all__ = [
     "qubit_subspace_op",
     "qudit_swap_op",
     "serialize_circuits",
+    "resource_counters",
     "validation",
 ]
--- cirq-superstaq/cirq_superstaq/resource_counters_test.py
+++ cirq-superstaq/cirq_superstaq/resource_counters_test.py
@@ -1,10 +1,21 @@
+# Copyright 2025 The Superstaq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from __future__ import annotations
 
 import cirq
 import cirq_superstaq as css
 import numpy as np
-
-import superstaq as ss
 
 
 def test_resource_counters() -> None:
@@ -19,8 +30,8 @@ def test_resource_counters() -> None:
         cirq.TOFFOLI(qubits[0], qubits[1], qubits[2]),
     )
 
-    assert ss.resource_counters.num_single_qubit_gates(circuit) == 3
-    assert ss.resource_counters.num_two_qubit_gates(circuit) == 2
-    assert ss.resource_counters.num_phased_xpow_subgates(circuit) == 2
-    assert ss.resource_counters.num_global_ops(circuit) == 2
-    assert np.isclose(ss.resource_counters.total_global_rgate_pi_time(circuit), 0.5)
+    assert css.resource_counters.num_single_qubit_gates(circuit) == 3
+    assert css.resource_counters.num_two_qubit_gates(circuit) == 2
+    assert css.resource_counters.num_phased_xpow_subgates(circuit) == 2
+    assert css.resource_counters.num_global_ops(circuit) == 2
+    assert np.isclose(css.resource_counters.total_global_rgate_pi_time(circuit), 0.5)
