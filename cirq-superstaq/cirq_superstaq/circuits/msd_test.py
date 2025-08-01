0b8f6f7135baec58a958d625cc13f47437823d1f
Kerry Zhou <kerry@calc.local>
2025-08-01 16:29:28 -0500

    Delete tomography tests

--- cirq-superstaq/cirq_superstaq/circuits/msd_test.py
+++ cirq-superstaq/cirq_superstaq/circuits/msd_test.py
@@ -15,10 +15,6 @@ def test_5_to_1_msd() -> None:
     assert (cirq.dirac_notation(magic_state)) == "(-0.33+0.33j)|0⟩ + 0.89|1⟩"
 
     density_matrix = cirq.density_matrix_from_state_vector(state_vector, indices=[0])
-    tomo_res = cirq.experiments.single_qubit_state_tomography(
-        sim, qubits[0], magic_state_circuit, 1000
-    )
-
     expected_density = numpy.array(
         [
             [0.21132484 + 0.0j, -0.28867513 + 0.28867513j],
@@ -27,8 +23,6 @@ def test_5_to_1_msd() -> None:
         dtype=numpy.complex64,
     )
     assert numpy.allclose(density_matrix, expected_density, rtol=1e-05)
-    assert numpy.allclose(expected_density, tomo_res.data, rtol=0.1)
-    assert numpy.allclose(density_matrix, tomo_res.data, rtol=0.1)
 
 
 def test_7_to_1_msd() -> None:
@@ -42,10 +36,6 @@ def test_7_to_1_msd() -> None:
     assert (cirq.dirac_notation(magic_state)) == "0.71|0⟩ + 0.71j|1⟩"
 
     density_matrix = cirq.density_matrix_from_state_vector(state_vector, indices=[0])
-    tomo_res = cirq.experiments.single_qubit_state_tomography(
-        sim, qubits[0], magic_state_circuit, 1000
-    )
-
     expected_density = numpy.array(
         [
             [0.50000006 + 0.0j, 0.0 - 0.50000006j],
@@ -54,8 +44,6 @@ def test_7_to_1_msd() -> None:
         dtype=numpy.complex64,
     )
     assert numpy.allclose(density_matrix, expected_density, rtol=1e-05)
-    assert numpy.allclose(expected_density, tomo_res.data, rtol=0.1)
-    assert numpy.allclose(density_matrix, tomo_res.data, rtol=0.1)
 
 
 def test_15_to_1_msd() -> None:
@@ -69,10 +57,6 @@ def test_15_to_1_msd() -> None:
     assert (cirq.dirac_notation(magic_state)) == "0.71|0⟩ + (0.5+0.5j)|1⟩"
 
     density_matrix = cirq.density_matrix_from_state_vector(state_vector, indices=[15])
-    tomo_res = cirq.experiments.single_qubit_state_tomography(
-        sim, qubits[15], magic_state_circuit, 1000
-    )
-
     expected_density = numpy.array(
         [
             [0.49999997 + 0.0j, 0.35355338 - 0.35355338j],
@@ -81,5 +65,3 @@ def test_15_to_1_msd() -> None:
         dtype=numpy.complex64,
     )
     assert numpy.allclose(density_matrix, expected_density, rtol=1e-05)
-    assert numpy.allclose(expected_density, tomo_res.data, rtol=0.1)
-    assert numpy.allclose(density_matrix, tomo_res.data, rtol=0.1)
