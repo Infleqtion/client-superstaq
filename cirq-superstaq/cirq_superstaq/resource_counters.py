34cf33004d68bd5fd29500c501393505ca7a8a7f
nati <nwtbekele@gmail.com>
2025-08-20 22:46:44 -0500

    manually fix bad headers

--- cirq-superstaq/cirq_superstaq/resource_counters.py
+++ cirq-superstaq/cirq_superstaq/resource_counters.py
@@ -1,4 +1,4 @@
-# Copyright 2025 The Superstaq Developers
+# Copyright 2025 Infleqtion
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
