4b839a65ff4e5e543ae7d149976e4a53bb097afd
rich <rich@super.tech>
2025-11-14 10:52:42 -0600

    more accurate examples

--- general-superstaq/general_superstaq/models_test.py
+++ general-superstaq/general_superstaq/models_test.py
@@ -44,50 +44,93 @@ def test_external_provider_credentials() -> None:
     assert credentials.cq_org_id == "456"
 
 
-def test_worker_task_results() -> None:
-    with pytest.raises(ValueError, match=r"must return both"):
-        _ = gss.models.WorkerTaskResults(
-            circuit_ref="1234",
+def test_worker_task_results_validation() -> None:
+    with pytest.raises(
+        pydantic.ValidationError,
+        match=r"Workers cannot return a status of awaiting_simulation.",
+    ):
+        gss.models.WorkerTaskResults(
+            circuit_ref="f76e84f7-0c65-4f0b-b2d7-14135db3900c",
+            status=gss.models.CircuitStatus.AWAITING_SIMULATION,
+        )
+
+    with pytest.raises(
+        pydantic.ValidationError,
+        match=r"Workers cannot return results unless status is COMPLETED",
+    ):
+        gss.models.WorkerTaskResults(
+            circuit_ref="f76e84f7-0c65-4f0b-b2d7-14135db3900c",
+            status=gss.models.CircuitStatus.RUNNING,
+            successful_shots=10,
+        )
+
+    with pytest.raises(
+        pydantic.ValidationError,
+        match=r"Workers cannot return results unless status is COMPLETED",
+    ):
+        gss.models.WorkerTaskResults(
+            circuit_ref="f76e84f7-0c65-4f0b-b2d7-14135db3900c",
+            status=gss.models.CircuitStatus.RUNNING,
+            measurements={},
+        )
+
+    with pytest.raises(
+        pydantic.ValidationError,
+        match=(
+            r"When status=COMPLETED the worker must return both the measurements and the number of "
+            "successful shots."
+        ),
+    ):
+        gss.models.WorkerTaskResults(
+            circuit_ref="f76e84f7-0c65-4f0b-b2d7-14135db3900c",
             status=gss.models.CircuitStatus.COMPLETED,
-            measurements={"000": [0, 1, 3], "101": [2]},
+            measurements={},
         )
 
-    with pytest.raises(ValueError, match=r"same length"):
-        _ = gss.models.WorkerTaskResults(
-            circuit_ref="1234",
+    with pytest.raises(
+        pydantic.ValidationError,
+        match=(
+            r"When status=COMPLETED the worker must return both the measurements and the number of "
+            "successful shots."
+        ),
+    ):
+        gss.models.WorkerTaskResults(
+            circuit_ref="f76e84f7-0c65-4f0b-b2d7-14135db3900c",
             status=gss.models.CircuitStatus.COMPLETED,
-            successful_shots=4,
-            measurements={"000": [0, 1, 3], "1010": [2]},
+            successful_shots=10,
         )
 
-    with pytest.raises(ValueError, match=r"valid bitstrings"):
-        _ = gss.models.WorkerTaskResults(
-            circuit_ref="1234",
+    with pytest.raises(
+        pydantic.ValidationError,
+        match=(r"Measurement keys must be valid bitstrings."),
+    ):
+        gss.models.WorkerTaskResults(
+            circuit_ref="f76e84f7-0c65-4f0b-b2d7-14135db3900c",
             status=gss.models.CircuitStatus.COMPLETED,
-            successful_shots=4,
-            measurements={"000": [0, 1, 3], "xyz": [2]},
+            successful_shots=10,
+            measurements={"a": [0], "b": [1, 2]},
         )
 
-    with pytest.raises(ValueError, match=r"Not all successful shots have a measurement"):
-        _ = gss.models.WorkerTaskResults(
-            circuit_ref="1234",
+    with pytest.raises(
+        pydantic.ValidationError,
+        match=(r"All measurement keys must have the same length."),
+    ):
+        gss.models.WorkerTaskResults(
+            circuit_ref="f76e84f7-0c65-4f0b-b2d7-14135db3900c",
             status=gss.models.CircuitStatus.COMPLETED,
-            successful_shots=4,
-            measurements={"000": [0, 3], "101": [2]},
+            successful_shots=10,
+            measurements={"101": [0], "01": [1, 2]},
         )
 
-    with pytest.raises(ValueError, match=r"cannot return a status"):
-        _ = gss.models.WorkerTaskResults(
-            circuit_ref="1234",
-            status=gss.models.CircuitStatus.AWAITING_COMPILE,
-        )
-
-    with pytest.raises(ValueError, match=r"cannot return results unless"):
-        _ = gss.models.WorkerTaskResults(
-            circuit_ref="1234",
-            status=gss.models.CircuitStatus.FAILED,
-            successful_shots=4,
-            measurements={"000": [0, 1, 3], "101": [2]},
+    with pytest.raises(
+        pydantic.ValidationError,
+        match=(r"Not all successful shots have a measurement."),
+    ):
+        gss.models.WorkerTaskResults(
+            circuit_ref="f76e84f7-0c65-4f0b-b2d7-14135db3900c",
+            status=gss.models.CircuitStatus.COMPLETED,
+            successful_shots=2,
+            measurements={"101": [0], "001": [1, 3]},
         )
 
     _ = gss.models.WorkerTaskResults(
