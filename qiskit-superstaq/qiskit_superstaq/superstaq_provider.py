d1788e6af8a969ac3263d5863c2264d7f20f20ce
richrines1 <85512171+richrines1@users.noreply.github.com>
2026-01-23 16:59:41 -0600

    support atol_map in qscout_compile (#1340)

--- qiskit-superstaq/qiskit_superstaq/superstaq_provider.py
+++ qiskit-superstaq/qiskit_superstaq/superstaq_provider.py
@@ -424,6 +424,8 @@ class SuperstaqProvider(gss.service.Service):
         base_entangling_gate: str = "xx",
         num_qubits: int | None = None,
         error_rates: SupportsItems[tuple[int, ...], float] | None = None,
+        atol: float = 1e-8,
+        atol_map: SupportsItems[tuple[int, ...], float] | None = None,
         **kwargs: Any,
     ) -> qss.compiler_output.CompilerOutput:
         """Compiles and optimizes the given circuit(s) for the QSCOUT trapped-ion testbed at
@@ -456,6 +458,15 @@ class SuperstaqProvider(gss.service.Service):
                 for gates acting on those qubits (for example `{(0, 1): 0.3, (1, 2): 0.2}`). If
                 provided, Superstaq will attempt to map the circuit to minimize the total error on
                 each qubit.
+            atol: Optional tolerance (trace distance bound) used for approximate compilation.
+                Superstaq will elide gates which can be approximated within the given tolerance by
+                identity operations.
+            atol_map: Optional dictionary assigning compilation tolerances to physical qubits, in
+                the form `{<qubit_indices>: <atol>, ...}` where `<qubit_indices>` is a tuple of
+                physical qubit indices (ints) and `<atol>` is an absolute tolerance (trace distance
+                bound) for gates acting on those qubits (for example `{(0, 1): 0.3, (1, 2): 0.2}`).
+                If provided, these tolerances will override `atol` for gates on the given qubits.
+                Omitted qubit pairs default to `atol`.
             kwargs: Other desired qscout_compile options.
 
         Returns:
@@ -475,6 +486,8 @@ class SuperstaqProvider(gss.service.Service):
             base_entangling_gate=base_entangling_gate,
             num_qubits=num_qubits,
             error_rates=error_rates,
+            atol=atol,
+            atol_map=atol_map,
             **kwargs,
         )
 
