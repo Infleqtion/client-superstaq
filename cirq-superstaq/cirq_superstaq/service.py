76eb15b6e07a21f090b70fe4c8969b2bb8d30c44
richrines1 <85512171+richrines1@users.noreply.github.com>
2026-04-06 16:15:53 -0500

    support fractional counts in jobs (#1365)
    some jobs may have non-integer "counts" (for example due to MEM). this
    pr updates various annotations to reflect this

--- cirq-superstaq/cirq_superstaq/service.py
+++ cirq-superstaq/cirq_superstaq/service.py
@@ -241,7 +241,7 @@ class Service(gss.service.Service):
         param_resolver: cirq.ParamResolverOrSimilarType = cirq.ParamResolver({}),
         method: str | None = None,
         **kwargs: Any,
-    ) -> dict[str, int]: ...
+    ) -> dict[str, float]: ...
 
     @overload
     def get_counts(
@@ -252,7 +252,7 @@ class Service(gss.service.Service):
         param_resolver: cirq.ParamResolverOrSimilarType = cirq.ParamResolver({}),
         method: str | None = None,
         **kwargs: Any,
-    ) -> list[dict[str, int]]: ...
+    ) -> list[dict[str, float]]: ...
 
     def get_counts(
         self,
@@ -262,7 +262,7 @@ class Service(gss.service.Service):
         param_resolver: cirq.ParamResolverOrSimilarType = cirq.ParamResolver({}),
         method: str | None = None,
         **kwargs: Any,
-    ) -> dict[str, int] | list[dict[str, int]]:
+    ) -> dict[str, float] | list[dict[str, float]]:
         """Runs circuit(s) on the Superstaq API and returns the result(s) as a `dict`.
 
         Args:
@@ -1159,7 +1159,9 @@ class Service(gss.service.Service):
             noise=noise_dict,
         )
 
-    def process_cb(self, job_id: str, counts: list[dict[str, int]] | None = None) -> dict[str, Any]:
+    def process_cb(
+        self, job_id: str | uuid.UUID, counts: Sequence[Mapping[str, float]] | None = None
+    ) -> dict[str, Any]:
         """Processes the data from the Cycle Benchmarking protocol.
         Generates SPAM and decay parameter estimations in addition to the process infidelity.
 
