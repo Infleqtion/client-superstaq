4aa1abdf8b3bac449a9fc16b447e0290702b7546
Dara Ogunkoya <dara.ogunkoya@infleqtion.com>
2026-08-13 16:51:03 -0500

    clarify definition in serialization _prepare_gate

--- qiskit-superstaq/qiskit_superstaq/serialization.py
+++ qiskit-superstaq/qiskit_superstaq/serialization.py
@@ -318,13 +318,18 @@ def _prepare_gate(gate: qiskit.circuit.Instruction) -> qiskit.circuit.Instructio
         return gate
 
     if isinstance(gate, qiskit.circuit.ControlledGate):
+        definition = (
+            qiskit.QuantumCircuit()
+            if gate._definition is None
+            else _prepare_circuit(gate._definition)
+        )
         return qiskit.circuit.ControlledGate(
             name=gate._name,
             num_qubits=gate.num_qubits,
             params=gate.params,
             label=gate.label,
             num_ctrl_qubits=gate.num_ctrl_qubits,
-            definition=_prepare_circuit(gate._definition),
+            definition=definition,
             ctrl_state=gate.ctrl_state,
             base_gate=_prepare_gate(gate.base_gate),
         )
